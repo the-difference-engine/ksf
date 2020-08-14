7119782b0eba55e257e8addb651d2ef40c62482a
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-13 19:35:20 -0500

    db connected to sequlize

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,6 +1,20 @@
 
 const express = require('express');
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://ryanmansfield:postgres@localhost:5432')
+
+async function test(){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    console.log('hi')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+ test();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
