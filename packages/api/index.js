855f481c5545352c7eedc2a78a6aa20b4b98ec31
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-13 23:24:47 -0500

    rename test function to testSequelizeConnection

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,19 +2,16 @@ const express = require('express');
 require('dotenv').config()
 const sequelize =  require( './helper/sequelize.js');
 
-async function test(){
+async function testSequelizeConnection(){
   try {
     await sequelize.authenticate();
     console.log()
     console.log('Connection has been established successfully.');
-    console.log('hi')
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
 }
-test();
-
- console.log(process.env.postgres_user)
+testSequelizeConnection();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
