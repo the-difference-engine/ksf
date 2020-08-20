378a7acff2fdaddd10ec11ca0272636166e6169a
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-08-20 18:44:57 -0500

    migration for grant cycle

---
+++ packages/api/migrations/20200820221533-create-grant-cycle.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Grant_Cycles', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+      },
+      openedOn: {
+        type: Sequelize.DATE,
+      },
+      closedOn: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Grant_Cycles');
+  },
+};
