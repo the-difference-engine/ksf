f753bece5aeb407dc8ad43492cdb6c4f5a5e7d56
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-08-24 14:02:09 -0500

    standardize model/table names to lowercase

--- packages/api/migrations/20200820005843-create-user.js
+++ packages/api/migrations/20200820005843-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   },
 };
--- packages/api/models/user.js
+++ packages/api/models/user.js
@@ -1,19 +1,18 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
+  class user extends Model {}
 
-  class User extends Model {
-  };
-
-  User.init({
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+  user.init(
+    {
+      email: DataTypes.STRING,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'user',
+    }
+  );
+  return user;
 };
