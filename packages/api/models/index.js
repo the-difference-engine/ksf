3699f181101af3878f8ab6438718270cfee7826d
Bryan Vanderhoof <bvanderhoof@heroku.com>
2020-08-24 20:34:25 -0500

    Use sequelize helper in the models index

--- packages/api/models/index.js
+++ packages/api/models/index.js
@@ -3,18 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const sequelize = require('../helper/sequelize.js');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
+// Generated code that instantiates the models
 fs
   .readdirSync(__dirname)
   .filter(file => {
