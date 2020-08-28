5f65f81f50009258e763ce318fb2fa7d0c5ff498
ddybas <ddybas@gmail.com>
2020-08-27 20:51:46 -0500

    Added comment with resources for creating future tests

--- packages/app/src/App.test.js
+++ packages/app/src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+//Todo - make this a Jest snapshot test - https://jestjs.io/docs/en/snapshot-testing
 xtest("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
