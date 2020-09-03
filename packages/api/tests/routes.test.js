c5f309e8aa7688a9a4a9eec1dfcda700ed54692e
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-09-02 20:42:23 -0500

    removed sample test file and updated routes test to make it more clear

--- packages/api/tests/routes.test.js
+++ packages/api/tests/routes.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../server')
-describe('Post Endpoints', () => {
-  it('should create a new post', async () => {
+describe('GET Endpoints', () => {
+  it('should create a new GET request', async () => {
     const res = await request(app)
       .get('/greeting')
       .send({
