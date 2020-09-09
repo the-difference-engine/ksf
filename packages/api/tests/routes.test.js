dd3e7735ef3eab11e790be7c114c29f0686b80a3
ryan mansfield <ryanmansfield86@gmail.com>
2020-09-09 18:57:20 -0500

    removed .send() from dummy test

--- packages/api/tests/routes.test.js
+++ packages/api/tests/routes.test.js
@@ -4,10 +4,6 @@ describe('GET Endpoints', () => {
   it('should create a new GET request', async () => {
     const res = await request(app)
       .get('/greeting')
-      .send({
-        userId: 1,
-        title: 'test is cool',
-      })
     expect(res.statusCode).toBe(200)
   })
 })
