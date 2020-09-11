997e170afd72bfad963cc713696f28ae977e5aca
ryan mansfield <ryanmansfield86@gmail.com>
2020-09-10 19:37:19 -0500

    Changed config const to look for config.js file instead of json file

--- packages/api/helper/sequelize.js
+++ packages/api/helper/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 
 let sequelize;
 if (config.url) {
