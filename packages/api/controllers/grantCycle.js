9e7a595ac9af861de63fa62e28e32e7c21bede96
Pavel Machuca <pavelmachuca12@gmail.com>
2020-10-07 23:47:07 -0400

    :beetle: only one grant cycle can be active * update and create controllers handle this

--- packages/api/controllers/grantCycle.js
+++ packages/api/controllers/grantCycle.js
@@ -23,20 +23,28 @@ const findByName = async (req, res) => {
 };
 const update = async (req, res) => {
   if (!req.body.id) {
-    return res.status(400).send('Missing id input');
+    return res.status(400).send('Missing ID input');
   }
 
-  const { id, name, openedOn, closedOn } = req.body;
+  const { id, name, openedOn, closedOn, isActive } = req.body;
   try {
-    const grant = await db.GrantCycle.findByPk(id);
+    const [grant, activeGrant] = await Promise.all([
+      db.GrantCycle.findByPk(id),
+      db.GrantCycle.findOne({
+        where: { isActive: true },
+      }),
+    ]);
     if (grant) {
       if (name) grant.name = name;
       if (openedOn) grant.openedOn = openedOn;
       if (closedOn) grant.closedOn = closedOn;
+      if (isActive) {
+        if (activeGrant) {
+          return res.status(400).send(`Active grant already exists: ${activeGrant.name}`);
+        }
+        grant.isActive = isActive;
+      }
       grant.save();
-      /*
-      memo: check for any cycles that are active
-      */
       return res.status(204).json(grant);
     }
     return res.status(404).send('Grant does not exist');
@@ -47,6 +55,11 @@ const update = async (req, res) => {
 
 const create = async (req, res) => {
   try {
+    if (req.body.isActive) {
+      const activeGrant = await db.GrantCycle.findOne({ where: { isActive: true } });
+      if (activeGrant) return res.status(400).send(`Active grant already exists: ${activeGrant.name}`);
+    }
+
     const grant = await db.GrantCycle.create({ ...req.body });
     // memo: what if grant is falsy?
     // would this get caught by the 500 handler?
@@ -58,4 +71,9 @@ const create = async (req, res) => {
   }
 };
 
-module.exports = { create, findAll, findByName, update };
+module.exports = {
+  create,
+  findAll,
+  findByName,
+  update,
+};
