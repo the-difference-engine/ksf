a6f44fd096307d03425219f691f03509bf1dd438
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-10-14 20:39:08 -0500

    changed some names in columns and updated model

--- packages/api/migrations/20201013014125-nomination_delete_columns.js
+++ packages/api/migrations/20201013014125-nomination_delete_columns.js
@@ -2,38 +2,58 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return Promise.all([
-      queryInterface.removeColumn('nominations', 'dateReceived'),
       queryInterface.removeColumn('nominations', 'providerFirstName'),
       queryInterface.removeColumn('nominations', 'providerLastName'),
+      queryInterface.removeColumn('nominations', 'relationship'),
+      queryInterface.removeColumn('nominations', 'patientFirstName'),
+      queryInterface.removeColumn('nominations', 'patientLastName'),
+      queryInterface.removeColumn('nominations', 'age'),
+
+      queryInterface.addColumn('nominations', 'providerName', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'patientName', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'representativeRelationship', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'patientAge', {
+        type: Sequelize.INTEGER,
+      }),
+      queryInterface.addColumn('nominations', 'representativePhoneNumber', {
+        type: Sequelize.INTEGER,
+      }),
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return Promise.all([
-      queryInterface.addColumn('nominations', ' dateReceived', {
-        type: Sequelize.DATE,
-      }),
       queryInterface.addColumn('nominations', ' providerFirstName', {
         type: Sequelize.STRING,
       }),
       queryInterface.addColumn('nominations', ' providerLastName', {
         type: Sequelize.STRING,
       }),
+      queryInterface.addColumn('nominations', 'relationship', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'patientFirstName', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'patientLastName', {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn('nominations', 'age', {
+        type: Sequelize.INTEGER,
+      }),
+
+      queryInterface.removeColumn('nominations', 'providerName'),
+      queryInterface.removeColumn('nominations', 'patientName'),
+      queryInterface.removeColumn('nominations', 'representativeRelationship'),
+      queryInterface.removeColumn('nominations', 'representativePhoneNumber'),
+      queryInterface.removeColumn('nominations', 'patientAge'),
     ]);
   },
 };
