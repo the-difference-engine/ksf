2d0f3f3abaf8074ee2a10de7695566b0e1baf42c
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-10-15 22:00:31 -0500

    added test to check for private domain email to model test

--- packages/api/tests/models/nomination.test.js
+++ packages/api/tests/models/nomination.test.js
@@ -2,15 +2,21 @@ const request = require('supertest');
 const app = require('../../server');
 const db = require('../../models');
 
-describe('The Nomination Model', () => {
+describe('nomination model email validation', () => {
   let nomination;
 
   afterAll(() => {
     nomination.destroy();
   });
 
-  it('flags public domain emails', async () => {
-    nomination = db.Nomination.build({ id: 'b5a27641-d76b-4ab0-9cef-8bf9eb9e8fab', emailAddress: "test@gmail.com"  });
+  it('makes publicEmailDomain true if nomination originates from public email domain', async () => {
+    nomination = db.Nomination.build({ emailAddress: 'test@gmail.com' });
+    nomination.save();
+    expect(nomination.publicEmailDomain).toBe(true);
+  });
+
+  it('makes publicEmailDomain false if nomination originates from non public email domain', async () => {
+    nomination = db.Nomination.build({ emailAddress: 'test@thedifferenceengine.io' });
     nomination.save();
     expect(nomination.publicEmailDomain).toBe(true);
   });
