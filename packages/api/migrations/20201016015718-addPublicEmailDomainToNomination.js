026bf2ba4fe7f3bf58a64cd505f60372e2cc845b
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-10-19 18:42:19 -0500

    migration ran, and test wording changed

--- packages/api/migrations/20201016015718-addPublicEmailDomainToNomination.js
+++ packages/api/migrations/20201016015718-addPublicEmailDomainToNomination.js
@@ -2,20 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.addColumn('nominations', 'publicEmailDomain', {
+      type: Sequelize.BOOLEAN,
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    return queryInterface.removeColumn('nominations', 'publicEmailDomain');
+  },
 };
--- packages/api/tests/models/nomination.test.js
+++ packages/api/tests/models/nomination.test.js
@@ -9,15 +9,15 @@ describe('nomination model email validation', () => {
     nomination.destroy();
   });
 
-  it('makes publicEmailDomain true if nomination originates from public email domain', async () => {
+  it('publicEmailDomain is true when nomination is sent from a free public domain', async () => {
     nomination = db.Nomination.build({ emailAddress: 'test@gmail.com' });
     nomination.save();
     expect(nomination.publicEmailDomain).toBe(true);
   });
 
-  it('makes publicEmailDomain false if nomination originates from non public email domain', async () => {
+  it('publicEmailDomain is false when nomination is sent from a private email', async () => {
     nomination = db.Nomination.build({ emailAddress: 'test@thedifferenceengine.io' });
     nomination.save();
-    expect(nomination.publicEmailDomain).toBe(true);
+    expect(nomination.publicEmailDomain).toBe(false);
   });
 });
