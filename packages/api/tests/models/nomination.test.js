0776322fe1c27c7e3f6ddee485c624cc9517dad1
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-10-21 18:08:27 -0500

    changed test teardown to aftereach

--- packages/api/tests/models/nomination.test.js
+++ packages/api/tests/models/nomination.test.js
@@ -5,7 +5,7 @@ const db = require('../../models');
 describe('nomination model email validation', () => {
   let nomination;
 
-  afterAll(() => {
+  afterEach(() => {
     nomination.destroy();
   });
 
