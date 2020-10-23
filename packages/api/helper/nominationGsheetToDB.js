8442646e786bdd0deaa0995f2d631f12ef3dd33d
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-10-22 20:49:29 -0500

    findOrCreate instead create

--- packages/api/helper/nominationGsheetToDB.js
+++ packages/api/helper/nominationGsheetToDB.js
@@ -35,28 +35,32 @@ module.exports = function gsheetToDB() {
       if (index !== 0) {
         console.log(nomination[1]);
 
-        db.Nomination.create({
-          dateReceived: nomination[0],
-          providerName: nomination[10],
-          providerPhoneNumber: nomination[11],
-          providerEmailAddress: nomination[12],
-          providerTitle: nomination[13],
-          hospitalName: nomination[18],
-          hospitalURL: nomination[19],
-          hospitalAddress: nomination[20],
-          hospitalCity: nomination[21],
-          hospitalState: nomination[22],
-          hospitalZipCode: nomination[23],
-          representativeName: nomination[24],
-          representativeEmailAddress: nomination[25],
-          representativePhoneNumber: nomination[26],
-          representativeRelationship: nomination[27],
-          patientName: nomination[29],
-          patientAge: nomination[30],
-          admissionDate: nomination[31],
-          dischargeDate: nomination[32],
-          patientDiagnosis: nomination[34],
-          amountRequestedCents: parseInt(nomination[37].replace(/\$/g, '')),
+        db.Nomination.findOrCreate({
+          where: {
+            dateReceived: nomination[0],
+            providerName: nomination[10],
+            providerPhoneNumber: nomination[11],
+            providerEmailAddress: nomination[12],
+            providerTitle: nomination[13],
+            hospitalName: nomination[18],
+            hospitalURL: nomination[19],
+            hospitalAddress: nomination[20],
+            hospitalCity: nomination[21],
+            hospitalState: nomination[22],
+            hospitalZipCode: nomination[23],
+            representativeName: nomination[24],
+            representativeEmailAddress: nomination[25],
+            representativePhoneNumber: nomination[26],
+            representativeRelationship: nomination[27],
+            patientName: nomination[29],
+            patientAge: nomination[30],
+            admissionDate: nomination[31],
+            dischargeDate: nomination[32],
+            patientDiagnosis: nomination[34],
+            amountRequestedCents: parseInt(
+              parseFloat(nomination[37].replace(/\$/g, '')) * 100
+            ),
+          },
         });
       }
     });
