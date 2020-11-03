afdfa6ce8ac977eb9698be141c93ffd6768edda6
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-11-02 18:33:58 -0600

    working on search page

--- packages/app/src/components/SearchBar/index.js
+++ packages/app/src/components/SearchBar/index.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { SearchResultDataContext } from '../../utils/SearchResults';
+import { SearchResultDataContext } from '../../utils/context/SearchResultsContext';
+import { NominationsDataContext } from '../../utils/context/NominationsContext';
 
 const SearchBar = () => {
   const [searhTerm, setSearchTerm] = useState(['hello search teem']);
   const [SearchResultData, setAllSearchResultData] = useContext(
     SearchResultDataContext
+  );
+  const [NominationsData, setNominationsData] = useContext(
+    NominationsDataContext
   );
 
   useEffect(() => {});
@@ -13,6 +17,7 @@ const SearchBar = () => {
     <>
       <div>{searhTerm[0]}</div>
       <div>{SearchResultData[0]}</div>
+      <div>{NominationsData[0]}</div>
     </>
   );
 };
