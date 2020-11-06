40d76bc4a15762389343cbd97f68db3dadf22573
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-11-05 19:16:53 -0600

    changed api call from context into the component for effect so api run when use effect is called/ search bar pops up

--- packages/app/src/utils/context/NominationsContext.js
+++ packages/app/src/utils/context/NominationsContext.js
@@ -4,29 +4,7 @@ import nominationsAPI from '../API/nominationsAPI';
 export const NominationsDataContext = createContext();
 
 export const NominationsDataProvider = (props) => {
-  const [NominationsData, setNominationsData] = useState([]);
-
-  useEffect(() => {
-    findAllNominations();
-  }, []);
-
-  function findAllNominations() {
-    nominationsAPI
-      .getNominations()
-      .then((res) => {
-        setNominationsData([res.data]);
-        console.log(
-          'woohoo! this should be the nominatin data from the nom context',
-          NominationsData
-        );
-      })
-      .catch((err) =>
-        console.log(
-          err,
-          '*******************************************************'
-        )
-      );
-  }
+  const [NominationsData, setNominationsData] = useState();
 
   return (
     <NominationsDataContext.Provider
