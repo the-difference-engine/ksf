bfba0a8f7bfa4a1fb20c06102f7d49755fb7cd03
bwalborn <walbornbryce@gmail.com>
2020-11-12 22:49:13 -0600

    Added healthprovider.js plus test

---
+++ packages/app/src/components/healthProvider/healthProvider.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+function HealthProvider(props) {
+    const fields = [
+        {
+          label: "Name",
+          value: props.providerName
+        },
+        {
+          label: "Email Address",
+          value: props.emailAddress
+        },
+        {
+          label: "Phone Number",
+          value: props.providerPhoneNumber
+        },
+        {
+            label: "Title",
+            value: props.providerTitle
+          },
+        //   {
+        //     label: "Email Validated",
+        //     value: props.emailValidated
+        //   },
+        //   {
+        //     label: "Public Email Domain",
+        //     value: props.publicEmailDomain  ??
+        //   },
+        {
+          label: "Patient Diagnosis",
+          value: props.patientDiagnosis
+        }];
+      return (
+        <div className={styles.main}>
+          <div className={styles.header}>
+            <label className={styles.bold}>Health Provider Information</label>
+          </div>
+          <div className={styles.content}>
+            {
+              fields.map((obj) => (<div key={obj.label}>
+                <label>{obj.label}</label>
+                <span>{obj.value}</span>
+              </div>))
+            }
+          </div>
+        </div>
+      );
+    }
+  
+  export default HealthProvider;
