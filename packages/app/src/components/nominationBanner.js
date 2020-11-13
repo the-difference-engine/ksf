cf87b7a647c4f73269bebf301be51e61369f21ea
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-11-12 21:15:53 -0600

    padding added to banner container

--- packages/app/src/components/nominationBanner.js
+++ packages/app/src/components/nominationBanner.js
@@ -1,19 +1,45 @@
 import React, {  } from 'react';
 
 const NominationBanner = (props) => {
-
-
   return (
-    <div className="nomination-banner">
-      <div className="nomination-name">
-        <p>Application <br></br> <span>lastname-state-abbrevation</span></p>
-        <button>Edit</button>
-      </div>
-      <div className="nomination-overview">
-        <p>HP Name <br></br> <span>{props.nomination.providerName}</span></p>
-        <p>Family Member Name <br></br> <span>{props.nomination.representativeName}</span></p>
-        <p>Created Date <br></br> <span>{props.nomination.createdAt}</span></p>
-        <p>Grant Amount Requested <br></br> <span>{props.nomination.amountRequestedCents}</span></p>
+
+    <div className="nomination-banner-container">
+      <div className="row">
+        <div className="column column-10 icon-container">
+          <img src="/ksflogo.png" alt="other"/>
+        </div>
+
+        <div className="column column-90 nomination-details">
+
+          <div className="row banner-top">
+            <div className="column name">
+              <p>Application</p>
+              <span><h1><strong>lastname-state-abbrevation</strong></h1></span>
+            </div>
+          </div>
+
+          <div className="row  banner-bottom">
+            <div className="column hp-name">
+              <p>HP Name</p>
+              <span><h2><strong>{props.nomination.providerName}</strong></h2></span>
+            </div>
+            <div className="column fam-name">
+              <p>Family Member Name</p>
+              <span><h2><strong>{props.nomination.representativeName}</strong></h2></span>
+            </div>
+            <div className="column created-at">
+              <p>Created Date</p>
+              <span><h2><strong>{props.nomination.createdAt}</strong></h2></span>
+            </div>
+            <div className="column amount">
+               <p>Grant Amount Requested</p>
+              <span><h2><strong>{props.nomination.amountRequestedCents}</strong></h2></span>
+            </div>
+          </div>
+        </div>
+        <div className="column column-10">
+          <button className="button button-outline edit-button">Edit</button>
+        </div>
       </div>
     </div>
   );
