00d783705eb07024bcdf35b62c32e63de18b7564
bwalborn <walbornbryce@gmail.com>
2020-11-12 19:44:08 -0600

    Added react-router-dom * removed boilerplate styles * login button is homepage

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -4,17 +4,24 @@ import GoogleBtn from "./components/GoogleBtn/GoogleBtn";
 import "./App.css";
 import FamilyMemberInfo from "./components/familyInformation/familyInformation";
 import family_info from "./components/familyInformation/dummyData";
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <GoogleBtn />
-        <div style={{ width: "40vw", margin: "0 auto" }}>
-          <FamilyMemberInfo {...family_info} />
-        </div>
-      </div>
-    </div >
+    <BrowserRouter>
+      <Switch>
+        <Route path='/applicationdetails'>
+          <div style={{ margin: "0 auto", backgroundColor: 'grey', padding: '2em'}}>
+            <FamilyMemberInfo {...family_info} />
+          </div>
+        </Route>
+        <Route path='/'>
+          <div className='App'>
+            <GoogleBtn />
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
