97b2b1cc79835770577b9977552ed3faa04361ae
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-11-16 15:51:52 -0600

    googlebtn moved to login page with route and style

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,24 +1,27 @@
-import React from "react";
-import GoogleBtn from "./components/GoogleBtn/GoogleBtn";
-import "./App.css";
-import FamilyMemberInfo from "./components/familyInformation/familyInformation";
-import family_info from "./components/familyInformation/dummyData";
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import FamilyMemberInfo from './components/familyInformation/familyInformation';
+import family_info from './components/familyInformation/dummyData';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
+import Login from './components/pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/applicationdetails'>
-          <div style={{ margin: "0 auto", backgroundColor: 'grey', padding: '2em'}}>
+        <Route path="/applicationdetails">
+          <div
+            style={{
+              margin: '0 auto',
+              backgroundColor: 'grey',
+              padding: '2em',
+            }}
+          >
             <FamilyMemberInfo {...family_info} />
           </div>
         </Route>
-        <Route path='/'>
-          <div className='App'>
-            <GoogleBtn />
-          </div>
-        </Route>
+        <Route exact path={'/login'} component={Login} />
       </Switch>
     </BrowserRouter>
   );
---
+++ packages/app/src/components/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleBtn from '../GoogleBtn/GoogleBtn';
+import './style.css';
+
+const Login = () => {
+  return (
+    <div className="googleButton">
+      <GoogleBtn />
+    </div>
+  );
+};
+
+export default Login;
