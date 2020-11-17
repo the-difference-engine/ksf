51d72070e16b4a0b4e6076d130d232747f78ea2c
Pavel Machuca <pavelmachuca12@gmail.com>
2020-11-16 21:53:53 -0500

    information displays on response grid * removed unused styling * removed comments Co-authored-by: Bryce Walborn <walbornbryce@gmail.com>

--- packages/app/src/components/healthProvider/healthProvider.test.js
+++ packages/app/src/components/healthProvider/healthProvider.test.js
@@ -3,7 +3,6 @@ import { render } from "@testing-library/react";
 import HealthProvider from './healthProvider';
 import dummyData from './dummyData';
 
-//Todo - make this a Jest snapshot test - https://jestjs.io/docs/en/snapshot-testing
 test("renders prop name familyRepresentativeName", () => {
   const { getByText } = render(<HealthProvider {...dummyData} />);
   const reg = new RegExp(dummyData.providerName, 'i')
