562ceee9c012fd2341cdc66ff884f4e550f238ea
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-11-19 21:09:13 -0600

    login test small change

--- packages/app/src/components/pages/Login/login.test.js
+++ packages/app/src/components/pages/Login/login.test.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
-// import Link from '../Link.react';
 import Login from "./index";
 
 it("Renders Command Center Login", () => {
