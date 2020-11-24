33830dda8b8691ee5081fd95ef1aa6a45a66b2c4
Hannah <Hannah.Chamorro@gmail.com>
2020-11-23 20:55:21 -0600

    Search results page (#142)
    * googlebtn moved to login page with route and style
    
    * search bar on landing page and padding
    
    * maping results from list returning tr and a little styling
    
    * no white search wrapper
    
    * home page route created, searchBar moved to home page, kebab style on class
    
    * search results card
    
    * redirect to application page if nomination found; styling on search bar
    
    * function custom  application names to data
    
    * searchbar to assign nomination names upon useeffect api call; search results page has link to application page
    
    * remove console table
    
    * removed un needed packes from result card

---
+++ packages/app/src/components/SearchResultsCard/index.js
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { SearchResultDataContext } from '../../utils/context/SearchResultsContext';
+import './style.css';
+
+const SearchResultsCard = () => {
+  const [SearchResultData, setSearchResultData] = useContext(
+    SearchResultDataContext
+  );
+  return (
+    <>
+      <section className="search-result-card">
+        <table className="search-result-table">
+          <thead>
+            <tr>
+              <th>Search Results</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Application Name</td>
+              <td>Provider's Name</td>
+              <td>Patient Name</td>
+              <td>Recieved Date </td>
+            </tr>
+            {SearchResultData
+              ? SearchResultData.map((result) => (
+                  <tr key={result.id}>
+                    <td>
+                      <Link to={`/nomination/${result.id}`}>
+                        {result.nominationName}
+                      </Link>
+                    </td>
+                    <td>{result.providerName}</td>
+                    <td>{result.patientName}</td>
+                    <td>{result.dateReceived}</td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
+};
+
+export default SearchResultsCard;
