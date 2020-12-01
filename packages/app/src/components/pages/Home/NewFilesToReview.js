c39aa5f50f23e4e4ff012b7f540b57df882931e6
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-12-01 13:39:03 -0600

    minor indentation adjustment

--- packages/app/src/components/pages/Home/NewFilesToReview.js
+++ packages/app/src/components/pages/Home/NewFilesToReview.js
@@ -16,33 +16,33 @@ const NewFilesToReview = () => {
               New Files To Review
             </tr>
           </thead>
-        <tbody>
-          <tr>
-            <td>Application Name</td>
-            <td>HP Name</td>
-            <td>Family Member Name</td>
-            <td>Received Date</td>
-            <td>Stage</td>
-          </tr>
-            {NominationsData
-              ?
-              NominationsData.filter(nominations =>
-                nominations.status === "received"
-              )
-              .map(nomination =>
-                  <tr key={nomination.id}>
-                    <td>{nomination.nominationName}</td>
-                    <td>{nomination.providerName}</td>
-                    <td>{nomination.providerName}</td>
-                    <td>{nomination.dateReceived}</td>
-                    <td>need stage info</td>
-                  </tr>
-              ) :
-            <h2>No New Nominations</h2>
-            }
-          </tbody>
-          </table>
-        </section>
+          <tbody>
+            <tr>
+              <td>Application Name</td>
+              <td>HP Name</td>
+              <td>Family Member Name</td>
+              <td>Received Date</td>
+              <td>Stage</td>
+            </tr>
+              {NominationsData
+                ?
+                NominationsData.filter(nominations =>
+                  nominations.status === "received"
+                )
+                .map(nomination =>
+                    <tr key={nomination.id}>
+                      <td>{nomination.nominationName}</td>
+                      <td>{nomination.providerName}</td>
+                      <td>{nomination.providerName}</td>
+                      <td>{nomination.dateReceived}</td>
+                      <td>need stage info</td>
+                    </tr>
+                ) :
+              <h2>No New Nominations</h2>
+              }
+            </tbody>
+        </table>
+      </section>
     </div>
   );
 };
