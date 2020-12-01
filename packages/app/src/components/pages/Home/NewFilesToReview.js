61f80bc857bd0d2cd028c0f8af8b1524b09de74a
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-11-30 21:06:37 -0600

    trying to display info

--- packages/app/src/components/pages/Home/NewFilesToReview.js
+++ packages/app/src/components/pages/Home/NewFilesToReview.js
@@ -1,25 +1,72 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { NominationsDataContext } from '../../../utils/context/NominationsContext';
 
-
 const NewFilesToReview = () => {
   const [NominationsData, setNominationsData] = useContext(
     NominationsDataContext
   );
+  const [newNominations, setNewNominations] = useState([])
 
-  const filteredNoms = NominationsDataContext.each((nomination) => {
-      return [
-       (nomination.providerName),
-        (nomination.applicationName),
-        (nomination.representativeName),
-        (nomination.receivedDate),
-      ].some((nom) => nom.includes(searchTerm));
-    });
-    setSearchResultData(filteredNoms);
+  const renderList = () => {
+    if(NominationsData) {
+      return NominationsData.forEach((nomination) => {
+        if (nomination.status === 'received') {
+          return (
+           nomination
+          );
+        }
+      });
+    }
+  };
+
+  // if(NominationsData) {
+  //   renderList(NominationsData)
+  // }
 
   return (
-    <h1>hi</h1>
+    <div className="flat-list">
+      {renderList()}
+    </div>
   );
+
+  // if (NominationsData) {
+  //   const filteredNoms = NominationsData.forEach((nomination) => {
+  //     if(nomination.status === "received") {
+  //       return [
+  //         (nomination.providerName),
+  //         (nomination.nominationName),
+  //         (nomination.representativeName),
+  //         (nomination.dateReceived),
+  //       ]
+  //     }
+  //   })
+  //   // return setNewNominations(filteredNoms)
+  //   // return filteredNoms
+  // }
+
+
+//   return (
+//     <div>
+//       <h1>
+//         {console.log(NominationsData)}
+//         New Files To Review
+//       </h1>
+//       <div>
+//         <ul>
+//           {/* {console.log(newNominations)} */}
+//           {newNominations.map(nomination => {
+//             console.log(nomination)
+//             return <li key={nomination.id}>
+//               <div>
+//                 HP Name
+//                 {nomination.providerName}
+//               </div>
+//             </li>
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
 };
 
 export default NewFilesToReview;
