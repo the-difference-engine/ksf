12cd7e3fdc723e53fb748829361bc516f294bab7
alaynetheodore5 <astheod@aol.com>
2020-12-02 21:05:09 -0600

    application stages styling

--- packages/app/src/components/applicationStages/ApplicationStages.js
+++ packages/app/src/components/applicationStages/ApplicationStages.js
@@ -1,13 +1,25 @@
 import React, { useState, useContext } from "react";
 import { ActiveNominationContext } from  '../../utils/context/ActiveNominationContext';
-
+import './style.css';
 const ApplicationStages = () => {
 
     const [activeNomination, setActiveNomination] = useContext( ActiveNominationContext );
+    
     return (
         <>
-        <p>Hello Application Stages</p>
-        {console.log(activeNomination)};
+        {console.log(activeNomination)}
+        <div className="nomination-bar-wrapper">
+            <div className="row">
+                <div className="column">&#10003;</div>
+                <div className="column">Awaiting HIPAA</div> 
+                <div className="column">HIPAA Verified</div>
+                <div className="column">Document Review</div>
+                <div className="column">Ready for Board Review</div>
+            </div>
+            <div>
+                <button>&#10003; Mark Stage as Complete</button>
+            </div>
+        </div>
         </>
     );
 };
