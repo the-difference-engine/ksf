1d219ccec22de52bb4b0e9497937ef3d41be12ce
alaynetheodore5 <astheod@aol.com>
2020-12-02 18:16:41 -0600

    index nomination provider

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -10,6 +10,8 @@ import Home from './components/pages/Home/index';
 import SearchResults from './components/SearchResultsCard';
 import { NominationsDataProvider } from './utils/context/NominationsContext';
 import { SearchResultDataProvider } from './utils/context/SearchResultsContext';
+import { ActiveNominationProvider } from './utils/context/ActiveNominationContext';
+
 import './App.css';
 ReactDOM.render(
   <Router>
@@ -17,21 +19,23 @@ ReactDOM.render(
       <Switch>
         <NominationsDataProvider>
           <SearchResultDataProvider>
-            <Route exact path={'/'} component={Home} />
-            <Route exact path={'/login'} component={Login} />
-            <Route exact path={'/searchresults'} component={SearchResults} />
-            <Route path="/nomination/:id" exact component={NominationShow} />
-            <Route path="/applicationdetails">
-              <div
-                style={{
-                  margin: '0 auto',
-                  backgroundColor: 'grey',
-                  padding: '2em',
-                }}
-              >
-                <FamilyMemberInfo {...family_info} />
-              </div>
-            </Route>
+            <ActiveNominationProvider>
+              <Route exact path={'/'} component={Home} />
+              <Route exact path={'/login'} component={Login} />
+              <Route exact path={'/searchresults'} component={SearchResults} />
+              <Route path="/nomination/:id" exact component={NominationShow} />
+              <Route path="/applicationdetails">
+                <div
+                  style={{
+                    margin: '0 auto',
+                    backgroundColor: 'grey',
+                    padding: '2em',
+                  }}
+                >
+                  <FamilyMemberInfo {...family_info} />
+                </div>
+              </Route>
+            </ActiveNominationProvider>
           </SearchResultDataProvider>
         </NominationsDataProvider>
       </Switch>
