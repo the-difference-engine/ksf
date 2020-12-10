13b291a1179aaf0e5a22ecee982484404dd98290
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-12-10 11:37:44 -0600

    active status styling in variable needs to be move to func; shorten advance stage()

--- packages/app/src/components/applicationStages/ApplicationStages.js
+++ packages/app/src/components/applicationStages/ApplicationStages.js
@@ -1,62 +1,83 @@
-import React, { useState, useContext } from "react";
-import { ActiveNominationContext } from  '../../utils/context/ActiveNominationContext';
+import React, { useState, useContext, useEffect } from 'react';
+import { ActiveNominationContext } from '../../utils/context/ActiveNominationContext';
 import './style.css';
 const ApplicationStages = () => {
+  const [activeNomination, setActiveNomination] = useContext(
+    ActiveNominationContext
+  );
+  const [status, setStatus] = useState([
+    'received',
+    'awaiting HIPPA',
+    'HIPPA verified',
+    'document review',
+  ]);
 
-    const [activeNomination, setActiveNomination] = useContext( ActiveNominationContext );
-    
-    function advanceStage(e) {
-        console.log(activeNomination.status);
-        if(activeNomination.status === "received") {
-            console.log("i am clicked");
-            let holderArr = activeNomination;
-            holderArr.status = "awaiting HIPPA"
-            setActiveNomination(holderArr);
-            console.log(holderArr.status);
-            console.log(activeNomination.status);
-        }
-        if(activeNomination.status === "awaiting HIPPA") {
-            console.log("i am clicked");
-            let holderArr = activeNomination;
-            holderArr.status = "HIPPA verified"
-            setActiveNomination(holderArr);
-            console.log(holderArr.status);
-            console.log(activeNomination.status);
-        }
-        if(activeNomination.status === "HIPPA verified") {
-            console.log("i am clicked");
-            let holderArr = activeNomination;
-            holderArr.status = "document review"
-            setActiveNomination(holderArr);
-            console.log(holderArr.status);
-            console.log(activeNomination.status);
-        }
-        if(activeNomination.status === "document review") {
-            console.log("i am clicked");
-            let holderArr = activeNomination;
-            holderArr.status = "ready for board review"
-            setActiveNomination(holderArr);
-            console.log(holderArr.status);
-            console.log(activeNomination.status);
-        }
+  useEffect(() => {}, [activeNomination]);
+
+  function advanceStage(e) {
+    console.log(activeNomination.status);
+    console.log(activeNomination);
+    if (activeNomination.status === 'received') {
+      return (activeNomination.status = 'awaiting HIPPA');
     }
-
-    return (
-        <>
-        {console.log(activeNomination)}
-        <div className="nomination-bar-wrapper">
-            <div className="nomination-stage-bar row">
-                <div className="column complete bar">&#10003;</div>
-                <div className="column active hippa-bar">Awaiting HIPAA<span className="arrow"></span></div> 
-                <div className="column awaiting bar">HIPAA Verified</div>
-                <div className="column awaiting bar">Document Review</div>
-                <div className="column awaiting bar">Ready for Board Review</div>
-                <div className="column"></div>
-                <div className="column complete-button"onClick={() => advanceStage()}>&#10003; Mark Stage as Complete</div>
-            </div>
+    if (activeNomination.status === 'awaiting HIPPA') {
+      return (activeNomination.status = 'HIPPA verified');
+    }
+    if (activeNomination.status === 'HIPPA verified') {
+      return (activeNomination.status = 'document review');
+    }
+    if (activeNomination.status === 'document review') {
+      return (activeNomination.status = 'ready for board review');
+    }
+  }
+  const statusEl = status.map((x, i) => (
+    <>
+      {status.indexOf(activeNomination.status) === i ? (
+        <div className="step current">
+          {console.log('*** :) ***')}
+          {console.log(i, 'index of map')}
+          {console.log(status.indexOf(activeNomination.status))}
+          <span>{x}</span>
         </div>
-        </>
-    );
-};
+      ) : status.indexOf(activeNomination.status) < i ? (
+        <div className="step">
+          <span>{x}</span>
+        </div>
+      ) : status.indexOf(activeNomination.status) > i ? (
+        <div className="step done">
+          <span>{x}</span>
+        </div>
+      ) : (
+        console.log(':(')
+      )}
+    </>
+  ));
 
+  function createStatusEl(name) {
+    const iOfAcitveNomStat = status.indexOf(activeNomination.status);
+    name = 'hello';
+    return <div>{name}</div>;
+  }
+
+  return (
+    <>
+      {console.log(activeNomination)}
+      <div className="container">
+        <div className="wrapper">
+          <div className="arrow-steps clearfix">{statusEl}</div>
+          <div>{createStatusEl()}</div>
+          <div className="nav clearfix">
+            <div
+              href="#"
+              className="next pull-right"
+              onClick={() => advanceStage()}
+            >
+              &#10003; Mark Stage as Complete
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default ApplicationStages;
