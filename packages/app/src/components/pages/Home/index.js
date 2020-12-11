0baf0f2f90b172df81ae5bec6b9aa840bb6e725b
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-12-10 19:55:22 -0600

    124 landing page home (#146)
    * added NewFilesToReview component to home page comonent
    
    * Search results page (#142)
    
    * googlebtn moved to login page with route and style
    
    * search bar on landing page and padding
    
    * maping results from list returning tr and a little styling
    
    * no white search wrapper
    
    * home page route created, searchBar moved to home page, kebab style on class
    
    * search results card
    
    * redirect to application page if nomination found; styling on search bar
    
    * function custom  application names to data
    
    * searchbar to assign nomination names upon useeffect api call; search results page has link to application page
    
    * remove console table
    
    * removed un needed packes from result card
    
    * trying to display info
    
    * new files to review displaying data
    
    * minor indentation adjustment
    
    * added see more button
    
    * Added CSS file and basic font sizing
    
    * new files to review styling
    
    * added snapshot test, and finished styling
    
    * slight changes to banner styling based on testing for demo. Remove p tag from td columns. Corrected info for one of the columns to show family member name
    
    * final touches, ready to be reviewed
    
    * re-run snapshot test for component
    
    * made requested changes to pr
    
    * moved td to app.css and finalized css changes
    
    * updated snapshot test file
    
    Co-authored-by: Ryan Mansfield <ryanmansfield86@gmail.com>
    Co-authored-by: Hannah <Hannah.Chamorro@gmail.com>

--- packages/app/src/components/pages/Home/index.js
+++ packages/app/src/components/pages/Home/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, {useEffect, useContext} from 'react';
 import SearchBar from '../../SearchBar';
+import NewFilesToReview from './NewFilesToReview';
+import states from 'us-state-codes';
+import nominationsAPI from '../../../utils/API/nominationsAPI';
+import { NominationsDataContext } from '../../../utils/context/NominationsContext';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV)
 
-const home = () => {
+const Home = () => {
+  const [NominationsData, setNominationsData] = useContext(
+    NominationsDataContext
+  );
+
+// TODO figure out way to garuantee global state is set before things render
+  useEffect(() => {
+    findAllNominations();
+  }, []);
+
+  const nomName = (n) => {
+    const lastName = n.patientName ? n.patientName.split(' ')[1] : '';
+    const state = states.getStateCodeByStateName(n.hospitalState);
+    return `${lastName}-${state}`;
+  };
+
+  function findAllNominations() {
+    nominationsAPI
+      .getNominations()
+      .then((res) => {
+        const nominations = res.data;
+        nominations.forEach((nomination) => {
+          nomination.nominationName = nomName(nomination);
+          nomination.dateReceived = new Date(
+            nomination.dateReceived
+          ).toLocaleDateString();
+        });
+        setNominationsData(nominations);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <>
-      <SearchBar />
+      <div>
+        <SearchBar />
+      </div>
+      <div>
+        <NewFilesToReview />
+      </div>
     </>
   );
 };
 
-export default home;
+export default Home;
