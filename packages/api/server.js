3094193efc0ced0d540c11bf86e68613469dbc91
lsysophia <59341273+lsysophia@users.noreply.github.com>
2020-12-14 19:22:29 -0600

    continue working on proxy set up

--- packages/api/server.js
+++ packages/api/server.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const routes = require('./routes');
 const cors = require('cors');
 const app = express();
+const gSheetsToDB = require('./helper/nominationGsheetToDB')
 
+gSheetsToDB()
 app.use(cors());
 app.use(bodyParser.json());
 // app.use(proxy('/', { target : 'http://localhost:8080/'}));
