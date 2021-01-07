bb849eb68defc38cfa788a909603f0aeaa07b014
lsysophia <59341273+lsysophia@users.noreply.github.com>
2021-01-06 18:22:21 -0600

    address add'l comments

--- packages/app/src/components/pages/Home/NewFilesToReview.js
+++ packages/app/src/components/pages/Home/NewFilesToReview.js
@@ -18,45 +18,47 @@ const NewFilesToReview = () => {
   }
 
   return (
-    <table className="new-files-table">
-      <thead>
-        <tr>
-          <td className="add-padding-left new-files-title">
-            <FontAwesomeIcon icon="file-image" color="green" />
-            <h1>New Files To Review</h1>
-          </td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td className="new-files-see-more">
-            <div onClick={handleClick}>
-              <FontAwesomeIcon icon={showAll ? "chevron-circle-up" : "chevron-circle-down"} />
-            </div>
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="home-new-files-headers">
-          <td className="add-padding-left"><h2><strong>Application Name</strong></h2></td>
-          <td><h2><strong>HP Name</strong></h2></td>
-          <td><h2><strong>Family Member Name</strong></h2></td>
-          <td><h2><strong>Received Date</strong></h2></td>
-          <td><h2><strong>Stage</strong></h2></td>
-          <td></td>
-        </tr>
-          {NominationsData
-            ?
-              conditionalNominationRender().map(nomination =>
-                <NewNomination nomination={nomination} key={nomination.id} />
-              )
-            :
-              <tr>
-                <td>no new nominations</td>
-              </tr>
-          }
-        </tbody>
-    </table>
+    <div className="new-files-container">
+      <table className="new-files-table">
+        <thead>
+          <tr>
+            <td className="add-padding-left new-files-title">
+              <FontAwesomeIcon icon="file-image" color="green" />
+              <h1>New Files To Review</h1>
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td className="new-files-see-more">
+              <div onClick={handleClick}>
+                <FontAwesomeIcon icon={showAll ? "chevron-circle-up" : "chevron-circle-down"} />
+              </div>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="home-new-files-headers">
+            <td className="add-padding-left"><h2><strong>Application Name</strong></h2></td>
+            <td><h2><strong>HP Name</strong></h2></td>
+            <td><h2><strong>Family Member Name</strong></h2></td>
+            <td><h2><strong>Received Date</strong></h2></td>
+            <td><h2><strong>Stage</strong></h2></td>
+            <td></td>
+          </tr>
+            {NominationsData
+              ?
+                conditionalNominationRender().map(nomination =>
+                  <NewNomination nomination={nomination} key={nomination.id} />
+                )
+              :
+                <tr>
+                  <td>No new nominations</td>
+                </tr>
+            }
+          </tbody>
+      </table>
+    </div>
   );
 };
 
