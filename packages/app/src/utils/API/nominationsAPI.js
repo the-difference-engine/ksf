aef5149642abe6e568fc2a62bcabc1af4aed87eb
lsysophia <59341273+lsysophia@users.noreply.github.com>
2021-01-07 19:12:37 -0600

    refine apiurl for nominationsAPI (#157)
    * refine apiurl for nominationsAPI
    
    * addressing comment
    
    * should be final update

--- packages/app/src/utils/API/nominationsAPI.js
+++ packages/app/src/utils/API/nominationsAPI.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL
 
+const cleanURL = (str) => {
+  if (str.endsWith('/')) {
+    return str.slice(0, -1);
+  }
+  return str
+}
+
 const nominationsAPI = {
   getNominations: function () {
-    return axios.get(`${API_URL}/api/nominations`);
+    return axios.get(`${cleanURL(API_URL)}/api/nominations`);
   },
   fetchNomination: function (id) {
-    return axios.get(`${API_URL}/api/nominations/${id}`);
+    return axios.get(`${cleanURL(API_URL)}/api/nominations/${id}`);
   },
   updateNomination: function (id, value) {
     return axios.put(`/nominations/${id}`, {
