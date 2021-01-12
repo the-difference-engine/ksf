d9fd7b044be5a1c386f9e94a96eaa3055ad4ade2
Pavel Machuca <pavelmachuca12@gmail.com>
2021-01-11 21:51:53 -0500

    added to new Nomination

--- packages/app/src/components/pages/Home/NewNomination.js
+++ packages/app/src/components/pages/Home/NewNomination.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const NewNomination = (nomination) => {
+const NewNomination = ({nomination}) => {
   if (nomination) {
+    console.log(nomination)
+    function handleClick() {
+      window.location.href = `/nomination/${nomination.id}`
+    }
+
     return (
-      <tr key={nomination.nomination.id}>
-        <td className="new-files-application-name add-padding-left detail-font-size">{nomination.nomination.nominationName}</td>
-        <td className="detail-font-size">{nomination.nomination.providerName}</td>
-        <td className="detail-font-size">{nomination.nomination.representativeName}</td>
-        <td className="detail-font-size">{nomination.nomination.dateReceived}</td>
-        <td className="detail-font-size">need stage info</td>
+      <tr key={nomination.id} onClick={handleClick}>
+        <td className="new-files-application-name add-padding-left detail-font-size">{nomination.nominationName}</td>
+        <td className="detail-font-size">{nomination.providerName}</td>
+        <td className="detail-font-size">{nomination.representativeName}</td>
+        <td className="detail-font-size">{nomination.dateReceived}</td>
+        <td className="detail-font-size">{nomination.status}</td>
         <td className="dot-fa-ellipsis"><FontAwesomeIcon icon="ellipsis-v"/></td>
       </tr>
     )
