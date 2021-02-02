aa517df195bc07003ae9122a5329863feb063632
lsysophia <59341273+lsysophia@users.noreply.github.com>
2021-02-01 21:12:03 -0600

    modifications to sync component and display messages

--- packages/app/src/components/syncNominations/syncNominations.js
+++ packages/app/src/components/syncNominations/syncNominations.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import nominationsAPI from '../../utils/API/nominationsAPI';
 import styles from './styles.css';
-import NominationsDataProvider from '../../utils/context/NominationsContext'
+import { findAllNominations } from '../../utils/context/NominationsContext'
 
 const SyncNominations = () => {
+  const [message, setMessage] = useState('')
+  // const [NominationsData, setNominationsData] = useContext(NominationsDataContext)
 
   const handleClick = () => {
+    getSyncNom()
+  }
+
+  // useEffect(() => {
+
+  // })
+
+  function removeMessage() {
+    setTimeout(() => {
+      setMessage('')
+    }, 2000)
+  }
+
+  function getSyncNom() {
     nominationsAPI.syncNominations()
+    .then((res) => {
+      if (res.status === 200) {
+        setMessage('Nominations successfully synced')
+        removeMessage()
+      } else {
+        setMessage('Error occurred while syncing')
+        removeMessage()
+      }
+    })
   }
 
   return (
-    <button onClick={handleClick} className='sync-noms'>Sync Nominations</button>
+    <>
+      <button onClick={handleClick} className='sync-noms'>Sync Nominations</button>
+      {
+        message ?
+        <div>
+          <h2>{message}</h2>
+        </div>
+        : null
+      }
+    </>
+
   )
 
 }
