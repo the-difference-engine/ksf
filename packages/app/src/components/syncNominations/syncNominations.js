8146f4eb81d208c8d07a20ed61dcbfb7ef37f06e
lsysophia <59341273+lsysophia@users.noreply.github.com>
2021-02-15 18:59:31 -0600

    nomination detail amount requested formatted

--- packages/app/src/components/syncNominations/syncNominations.js
+++ packages/app/src/components/syncNominations/syncNominations.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import nominationsAPI from '../../utils/API/nominationsAPI';
 import styles from './styles.css';
 
