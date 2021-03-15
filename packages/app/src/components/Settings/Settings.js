4a9a112e6c03daf9bf8e1a990e033b7d7ad48c99
Saif Zahedi <saif.zahedi@gmail.com>
2021-03-15 16:59:52 -0500

    Create and style grant cycle settings component

--- packages/app/src/components/Settings/Settings.js
+++ packages/app/src/components/Settings/Settings.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import 'react-day-picker/lib/style.css';
+import grantCycleAPI from '../../utils/API/grantCycleAPI';
 import './styles.css';
 
 const Settings = () => {
+    const [newGrantCycle, setNewGrantCycle] = useState({openedOn: "", closedOn: ""});
+    const [allGrantCycles, setGrantCycles] = useState([]);
+    const [disableButton, setDisableButton] = useState(true);
+    const [errors, setErrors] = useState("");
+
+    async function getGrantCycles() {
+        const { data } = await grantCycleAPI.getGrantCycles();
+        setGrantCycles(data);
+    }
+
+    useEffect(() => {
+        getGrantCycles();
+    }, []);
+
+    const handleChange = ({currentTarget: input}) => {
+        const grantCycle = {...newGrantCycle};
+        grantCycle[input.name] = input.value;
+        setNewGrantCycle(grantCycle);
+
+        if (new Date(grantCycle.openedOn) && new Date(grantCycle.closedOn)) {
+            const millisecondsInADay = 86400000;
+            const startDate = new Date(grantCycle.openedOn);
+            const endDate = new Date(grantCycle.closedOn);
+            const result = endDate - startDate >= millisecondsInADay ? false : true;
+            
+            setDisableButton(result);
+        }
+    }
+
+    const handleCreate = async () => {
+        try{
+            const {data} = await grantCycleAPI.createGrantCycle(newGrantCycle);
+            setNewGrantCycle({openedOn: "", closedOn: ""})
+            getGrantCycles();
+        }
+        catch(ex) {
+            if (ex.response && ex.response.status === 400) {
+                setErrors(ex.response.data);
+            }
+        }
+    }
+
+    const formatDateString = date => {
+        const year = date.slice(0, 4);
+        const month = date.slice(5, 7);
+        const day = date.slice(8, 10);
+        const result = `${month}-${day}-${year}`;
+        return result;
+    }
+
     return ( 
         <div className="settings-container">
             <header className="settings-header">
@@ -21,21 +69,34 @@ const Settings = () => {
                 </aside>
                 <section className="settings-section">
                     <h2 className="settings-section-heading">Create Grant Cycle</h2>
-                    <div>
+                    <div className="settings-form">
                         <div className="settings-input-block">
                             <p>Start Date:</p>
                             <span className="settings-input">
-                                <input className="settings-input" type="date"/>
+                                <input
+                                    value={newGrantCycle.openedOn}
+                                    name="openedOn"
+                                    onChange={handleChange}
+                                    
+                                    type="date"
+                                />
                             </span>
                         </div>
                         <div className="settings-input-block">
                             <p>End Date:</p>
                             <span className="settings-input">
-                                <input className="settings-input" type="date"/>
+                                <input
+                                    value={newGrantCycle.closedOn}
+                                    name="closedOn"
+                                    onChange={handleChange}
+                                    
+                                    type="date"
+                                />
                             </span>
                         </div>
                     </div>
-                    <button className="settings-button">Create</button>
+                    <button disabled={disableButton} onClick={handleCreate} className="settings-button">Create</button>
+                    <div className="settings-form-errors">{ errors }</div>
                     <h2 className="settings-section-heading">Grant Cycle History</h2>
                     <table className="settings-table">
                         <thead className="settings-thead">
@@ -46,26 +107,13 @@ const Settings = () => {
                             </tr>
                         </thead>
                         <tbody className="settings-tbody">
-                            <tr>
-                                <td>01/01/2021</td>
-                                <td>03/31/2021</td>
-                                <td className="settings-cell">4</td>
-                            </tr>
-                            <tr>
-                                <td>10/01/2020</td>
-                                <td>12/31/2020</td>
-                                <td className="settings-cell">14</td>
-                            </tr>
-                            <tr>
-                                <td>07/01/2020</td>
-                                <td>09/30/2020</td>
-                                <td className="settings-cell">22</td>
-                            </tr>
-                            <tr>
-                                <td>04/01/2020</td>
-                                <td>06/30/2020</td>
-                                <td className="settings-cell">17</td>
-                            </tr>
+                            {allGrantCycles.map(gc => (
+                                <tr key={gc.id}>
+                                    <td>{formatDateString(gc.openedOn)}</td>
+                                    <td>{formatDateString(gc.closedOn)}</td>
+                                    <td>{gc.count}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </section>
