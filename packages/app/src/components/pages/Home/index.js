ae6b4d7a14004d8a567285a716f0fcb5057fc7fe
Saif Zahedi <saif.zahedi@gmail.com>
2021-03-18 16:08:44 -0500

    Update imports for additional icons

--- packages/app/src/components/pages/Home/index.js
+++ packages/app/src/components/pages/Home/index.js
@@ -5,9 +5,9 @@ import SyncNominations from '../../syncNominations/syncNominations'
 import { NominationsDataContext } from '../../../utils/context/NominationsContext';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV, faCog, faTimes, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV, faCog, faTimes, faPencilAlt, faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import ApplicationViewByStages from './ApplicationViewByStages';
-library.add(fab, faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV, faCog, faTimes, faPencilAlt)
+library.add(fab, faChevronCircleDown, faChevronCircleUp, faFileImage, faEllipsisV, faCog, faTimes, faPencilAlt, faArrowRight, faArrowLeft)
 
 const Home = () => {
   const [NominationsData, setNominationsData] = useContext(NominationsDataContext);
