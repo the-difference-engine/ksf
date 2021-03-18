b6a64e986d633f8a768b1496ea3ed95a01df8eec
Austin Schey <aschey13@gmail.com>
2021-03-17 20:19:02 -0500

    don't use url for migrations because that circumvents the config file (#218)

--- packages/api/helper/sequelize.js
+++ packages/api/helper/sequelize.js
@@ -1,13 +1,10 @@
 const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-let dbUrl = process.env.DATABASE_URL;
+const dbUrl = process.env.DATABASE_URL;
 
 let sequelize;
 if (dbUrl) {
-  if (env === 'production') {
-    dbUrl += '?sslmode=require';
-  }
   sequelize = new Sequelize(dbUrl, config);
 } else {
   sequelize = new Sequelize(
