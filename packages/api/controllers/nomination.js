2cc144a052f84556daf933e5423ee1d30e31f20d
Bryce Walborn <Walbornbryce@gmail.com>
2021-04-01 21:21:20 -0500

    173 Verify Email Address (#204)
    * 173 Verify Email Address
    
    * added jsonwebtoken. Made generatetoken.js
    
    * emailverification in controller/nomination.js
    
    * added nominations GET route for email verification
    
    * Minor fix - properly exporting generateToken module
    
    * Fixed naming issue that was causing an error.
    
    * renamed 'email' to 'emailAddress'
    
    * properly imported generateToken into mailer.js
    
    * added console.log() in nomination.test.js
    
    * updated mailer.js and nomination.test.js
    
    * adding emailValidated to nomination.test.js
    
    * removing console logs
    
    * Added working tests -> nomination.test.js
    
    * *minor fix
    
    * testing with hardcoded mock secret
    
    * updating .github/workflow -> JWT_SECRET
    
    * reformate .github file
    
    * rerun .github file

--- packages/api/controllers/nomination.js
+++ packages/api/controllers/nomination.js
@@ -1,11 +1,10 @@
 const { validate: uuidValidate } = require('uuid');
 const { ValidationError } = require('sequelize');
 const db = require('../models');
-const { sendDeclineEmail } = require('../helper/mailer')
-const { sendSurveyEmail } = require('../helper/mailer')
-const { verifyHcEmail } = require('../helper/mailer')
-const gsheetToDB = require('../helper/nominationGsheetToDB')
-
+const { sendDeclineEmail } = require('../helper/mailer');
+const { verifyHcEmail } = require('../helper/mailer');
+const gsheetToDB = require('../helper/nominationGsheetToDB');
+const jwt = require('jsonwebtoken');
 
 const getNominationById = async (req, res) => {
   try {
@@ -20,9 +19,7 @@ const getNominationById = async (req, res) => {
     if (nomination) {
       return res.status(200).json({ nomination });
     }
-    return res
-      .status(404)
-      .send('Nomination with the specified ID does not exist!');
+    return res.status(404).send('Nomination with the specified ID does not exist!');
   } catch (error) {
     console.error('500 - something is not right', error);
     return res.status(500).send(error.message);
@@ -47,11 +44,11 @@ const createNomination = async (req, res) => {
     const { providerEmailAddress } = req.body;
     const newNomination = await db.Nomination.create(req.body);
     const nominations = await db.Nomination.findAll();
-    const hasProviderBeenValidated = nominations.some( (nom) => {
-      return nom.providerEmailAddress === providerEmailAddress && nom.emailValidated === true
-    })
-    if(!hasProviderBeenValidated) {
-      verifyHcEmail(newNomination.dataValues)
+    const hasProviderBeenValidated = nominations.some((nom) => {
+      return nom.providerEmailAddress === providerEmailAddress && nom.emailValidated;
+    });
+    if (!hasProviderBeenValidated) {
+      verifyHcEmail(newNomination.dataValues);
     }
     return res.status(201).json({ newNomination });
   } catch (error) {
@@ -68,35 +65,33 @@ const updateNomination = async (req, res) => {
   const { id } = req.params;
   try {
     const nomination = await db.Nomination.findOne({
-      where: { id }
-    })
-    nomination.update(
-      { status: req.body.status }
-    ).catch ((err)=> {
-      console.log('Nomination Not Found', err)
-      return res.status(400)});
+      where: { id },
+    });
+    nomination.update({ status: req.body.status }).catch((err) => {
+      console.log('Nomination Not Found', err);
+      return res.status(400);
+    });
     //can continue using additional conditional to use other email functions,
     //depending on status of application
     //current nominations don't have decline status, that should come after nominations hit ready for board review. TBD
     if (nomination.changed('status')) {
-      
       if (nomination.status === 'Decline') {
-        sendDeclineEmail(nomination)
+        sendDeclineEmail(nomination);
       }
-      
+
       if (nomination.status === 'HIPAA Verified') {
         try {
-          nomination.update(
-            { hipaaTimestamp: Date() }
-          ).catch ((err)=> {
-            console.log('Nomination Not Found', err)
-            return res.status(400)});
-          }
-        finally { sendSurveyEmail(nomination); }
+          nomination.update({ hipaaTimestamp: Date() }).catch((err) => {
+            console.log('Nomination Not Found', err);
+            return res.status(400);
+          });
+        } finally {
+          sendSurveyEmail(nomination);
+        }
       }
-      
+
       return res.status(200).json(nomination);
-    } 
+    }
   } catch (error) {
     console.log('400 Update Bad Request', error);
     return res.status(400).json({ error: error.message });
@@ -104,14 +99,27 @@ const updateNomination = async (req, res) => {
 };
 
 const syncNominations = async (req, res) => {
-  try { gsheetToDB()
-    console.log('nominations synced successfully')
-  return res.status(200).json({ 'status': 'ok' })
-} catch (error){
-  console.log('error', error)
-  return res.status(400).json({ error: error.message });
+  try {
+    gsheetToDB();
+    console.log('nominations synced successfully');
+    return res.status(200).json({ status: 'ok' });
+  } catch (error) {
+    console.log('error', error);
+    return res.status(400).json({ error: error.message });
   }
-}
+};
+
+const emailVerifiction = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { nomination: id } = jwt.verify(token, process.env.JWT_SECRET);
+    await db.Nomination.update({ emailValidated: true }, { where: { id } });
+    return res.status(200).json({ status: 'ok' });
+  } catch (error) {
+    console.log('400 validation error', error);
+    return res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   getNominationById,
@@ -119,5 +127,5 @@ module.exports = {
   createNomination,
   updateNomination,
   syncNominations,
+  emailVerifiction,
 };
-
