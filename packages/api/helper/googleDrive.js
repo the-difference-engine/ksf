7c39c51c053e0a3f7300632a2d401e949b9e9c1b
Abraham Herrera <50112365+municipalvice@users.noreply.github.com>
2021-04-08 19:45:33 -0500

    Ticket195 (#226)
    * message
    
    * Drive API running as script
    
    * Attached folder creation to front end
    
    * Service Account Folders
    
    * Service account creates folder under Applications folder
    
    * Clean up
    
    * Google Folder now created with application name
    
    * Refactored to move creds to .env
    
    * Removed unecessary .gitignore entry
    
    * Fixed random test.js file
    
    * Refactored, helper parses private keys
    
    * Fixed issue with parsefunction
    
    * Trying to fix a failed check
    
    Co-authored-by: jhartnettpro <jhartnettpro@gmail.com>

---
+++ packages/api/helper/googleDrive.js
@@ -0,0 +1,34 @@
+const { google } = require('googleapis');
+const parsePrivateKey = require('./parsePrivateKey');
+const parentFolderId = process.env.APPLICATION_FOLDER_ID
+const clientEmail = process.env.SERVICE_CLIENT_EMAIL
+const privateKey = parsePrivateKey(process.env.SERVICE_PRIVATE_KEY)
+const scopes = [
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.appdata',
+  'https://www.googleapis.com/auth/drive.file'
+];
+
+module.exports = function createFolder(applicationName) {
+
+  const auth = new google.auth.JWT(
+    clientEmail, null,
+    privateKey, scopes
+  );
+
+  const drive = google.drive({ version: 'v3', auth });
+  let fileMetadata = {
+    'name': applicationName,
+    'parents': [parentFolderId],
+    'mimeType': 'application/vnd.google-apps.folder'
+  };
+  drive.files.create({
+    resource: fileMetadata,
+    fields: 'id'
+  }, function (err, file) {
+    if (err) {
+      // Handle error
+      console.error(err);
+    }
+  })
+}
