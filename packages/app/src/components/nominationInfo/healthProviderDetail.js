cd46ca453af02b094f266f9099ac49102901e056
Jon Westbrook <jon.westbrook.jr@gmail.com>
2021-04-19 18:31:30 -0500

    Health provider detail reset to dev

--- packages/app/src/components/nominationInfo/healthProviderDetail.js
+++ packages/app/src/components/nominationInfo/healthProviderDetail.js
@@ -11,27 +11,34 @@ function HealthProviderDetail(props) {
       window.open('#');
   };
 
-      return (
-        <div className={styles.main}>
-          <div className={styles.header}>
-          <label className={styles.bold}>{props.title}</label>
-          </div>
-          <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
-            {
-              props.propsData.map((obj, index) =>  obj.label === "Provider Name" ? (
-                <div key={index} className={obj.label === "" ? styles.mobileHide: ""}>
-                      <label className={styles.label}>{obj.label}</label>
-                        <Link to={Switch} onClick={() => handleSubmit(obj.value)}  key={index}>
-                          <span className={styles.value}>{String(obj.value)}</span>
-                        </Link></div>) 
-                      : (<div key={index} className={obj.label === "" ? styles.mobileHide: ""}>
-                            <label className={styles.label}>{obj.label}</label>
-                            <span className={styles.value}>{String(obj.value)}</span>
-                        </div>))
-            }
-          </div>
-        </div>
-      );
-    }
-  
-  export default HealthProviderDetail;
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <label className={styles.title}>{props.title}</label>
+      </div>
+      <div className={[styles.content, props.gridContent && styles['grid-container']].join(' ')}>
+        {props.fields.map((obj, index) =>
+          obj.label === 'Provider Name' ? (
+            <div key={index} className={obj.label === '' ? styles.mobileHide : ''}>
+              <label className={styles.label}>{obj.label}</label>
+              
+                <Link to={`/searchhealthprovider/${obj.value}`}>
+                  <span className={styles.value} onClick={() => openWindow()} key={index}>
+                    {obj.value}
+                  </span>
+                </Link>
+              
+            </div>
+          ) : (
+            <div key={index} className={obj.label === '' ? styles.mobileHide : ''}>
+              <label className={styles.label}>{obj.label}</label>
+              <span className={styles.value}>{obj.value}</span>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default HealthProviderDetail;
