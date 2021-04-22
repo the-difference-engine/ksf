9ed5547e03ac3affceeb7b864f427ac6fb1f5a8b
bwalborn <walbornbryce@gmail.com>
2021-04-21 21:05:14 -0500

    Adding Decline App button

--- packages/app/src/components/nominationBanner/nominationBanner.js
+++ packages/app/src/components/nominationBanner/nominationBanner.js
@@ -1,52 +1,79 @@
-import style from './style.css'
+import style from './style.css';
 import React from 'react';
 const states = require('us-state-codes');
 
 const NominationBanner = ({ nomination }) => {
-  const date = new Date(nomination.dateReceived).toLocaleDateString()
-  const lastName = nomination.patientName ? nomination.patientName.split(' ')[1] : ''
-  const state = states.getStateCodeByStateName(nomination.hospitalState)
-  const nominationName = `${lastName}-${state}`
-  const formattedAmount = nomination.amountRequestedCents ? (nomination.amountRequestedCents).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') : ''
+  const date = new Date(nomination.dateReceived).toLocaleDateString();
+  const lastName = nomination.patientName ? nomination.patientName.split(' ')[1] : '';
+  const state = states.getStateCodeByStateName(nomination.hospitalState);
+  const nominationName = `${lastName}-${state}`;
+  const formattedAmount = nomination.amountRequestedCents ? nomination.amountRequestedCents.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') : '';
 
+  const handleDecline = () => {};
 
   return (
     <div className="nomination-banner-container">
       <div className="row">
         <div className="column column-10 icon-container">
-          <img src="/ksflogo.png" alt="other"/>
+          <img src="/ksflogo.png" alt="other" />
         </div>
 
         <div className="column column-80 nomination-details">
-
           <div className="row banner-top">
-            <div className="column name">
+            <div className="column name" style={{ width: '20%' }}>
               <p>Application</p>
-              <span><h1 className="nom-name"><strong>{nominationName}</strong></h1></span>
+              <span>
+                <h1 className="nom-name">
+                  <strong>{nominationName}</strong>
+                </h1>
+              </span>
+            </div>
+            <div className="column name">
+              <button className=" decline-button" style={{ background: '#f72314', color: '#ffffff', outlineColor: '#e8e8e8', fontWeight: 'bold' }}>
+                Decline Application
+              </button>
             </div>
           </div>
 
           <div className="row">
             <div className="column hp-name">
               <p className="secondary-dark">HP Name</p>
-              <span><h2 className="body-font"><strong>{nomination.providerName}</strong></h2></span>
+              <span>
+                <h2 className="body-font">
+                  <strong>{nomination.providerName}</strong>
+                </h2>
+              </span>
             </div>
             <div className="column fam-name">
               <p className="secondary-dark">Family Member Name</p>
-              <span><h2 className="body-font"><strong>{nomination.representativeName}</strong></h2></span>
+              <span>
+                <h2 className="body-font">
+                  <strong>{nomination.representativeName}</strong>
+                </h2>
+              </span>
             </div>
             <div className="column created-at">
               <p className="secondary-dark">Submission Date</p>
-              <span><h2 className="body-font"><strong>{date}</strong></h2></span>
+              <span>
+                <h2 className="body-font">
+                  <strong>{date}</strong>
+                </h2>
+              </span>
             </div>
             <div className="column amount">
-               <p className="secondary-dark">Grant Amount Requested</p>
-              <span><h2 className="body-font"><strong>{formattedAmount ? `$${formattedAmount}` : ''}</strong></h2></span>
+              <p className="secondary-dark">Grant Amount Requested</p>
+              <span>
+                <h2 className="body-font">
+                  <strong>{formattedAmount ? `$${formattedAmount}` : ''}</strong>
+                </h2>
+              </span>
             </div>
           </div>
         </div>
         <div className="column column-10">
-          <button className="button button-outline edit-button" id="edit-button">Edit</button>
+          <button className="button button-outline edit-button" id="edit-button">
+            Edit
+          </button>
         </div>
       </div>
     </div>
