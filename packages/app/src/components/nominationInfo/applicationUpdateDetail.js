44c5ab5e46d7ff90bd74d82ce49043449897aaaa
Jon Westbrook <jon.westbrook.jr@gmail.com>
2021-04-29 18:32:17 -0500

    Addition of Formik/Yup libraries for validation.

--- packages/app/src/components/nominationInfo/applicationUpdateDetail.js
+++ packages/app/src/components/nominationInfo/applicationUpdateDetail.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import styles from "./styles.module.css";
 import nominationsAPI from "./../../utils/API/nominationsAPI.js";
 import { ActiveNominationContext } from '../../utils/context/ActiveNominationContext';
+import { useFormik } from "formik"; // npm install formik --save
+import * as Yup from 'yup'; // npm install yup --save
 
 /**
  * Function which is called to update the current active nomination's data fields.
@@ -11,54 +13,72 @@ import { ActiveNominationContext } from '../../utils/context/ActiveNominationCon
  */
 function ApplicationUpdateDetail(props) {
     const [activeNomination, setActiveNomination] = useContext(ActiveNominationContext);
-    let saveConfirm = false;
-    // Directly manipulates corresponding props values based on if statement checks.
-    const handleChange = (e) => {
-        // Prevents infinite change loop.
-        e.preventDefault();
-        saveConfirm = true;
 
-        // e.target.name checks props obj name string
-        if (e.target.name === 'Admission Date') {
-            props.propsData[2].value = e.target.value;
-        }
-
-        if (e.target.name === 'Discharge Date') {
-            props.propsData[3].value = e.target.value;
-        }
-
-        if (e.target.name === 'Representative Name') {
-            props.propsData[0].value = e.target.value;
-        }
-
-        if (e.target.name === 'Email Address') {
-            props.propsData[1].value = e.target.value;
-        }
-
-        if (e.target.name === 'Phone Number') {
-            props.propsData[2].value = e.target.value;
-        }
-
-        if (e.target.name === 'Relationship') {
-            props.propsData[3].value = e.target.value;
-        }
-
-        if (e.target.name === 'Request to communicate in Spanish?') {
-            props.propsData[4].value = e.target.value;
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        // Tries to update database with an Axios call, catches error if one occurs.
-        if (saveConfirm === true) {
-            try {
-                nominationsAPI.updateActiveNomData(activeNomination.id, props.propsData);
-                console.log("success");
-            } catch (err) {
-                console.log(err);
+    // Formik object which handles form updates and submits changes.
+    const patientEditForm = () => {
+        const formik = useFormik({
+            initialValues: {
+                'Admission Date': '',
+                'Discharge Date': '',
+                'Representative Name': '',
+                'Email Address': '',
+                'Phone Number': '',
+                Relationship: '',
+                'Request to communicate in Spanish?': '',
             }
-        }
+        })
     }
+    // let saveConfirm = false;
+
+    // // Directly manipulates corresponding props values based on if statement checks.
+    // // TODO: Need to figure out data validation in the form.
+    // const handleChange = (e) => {
+    //     // Prevents infinite change loop.
+    //     e.preventDefault();
+    //     saveConfirm = true;
+
+    //     // e.target.name checks props obj name string
+    //     if (e.target.name === 'Admission Date') {
+    //         props.propsData[2].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Discharge Date') {
+    //         props.propsData[3].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Representative Name') {
+    //         props.propsData[0].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Email Address') {
+    //         props.propsData[1].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Phone Number') { 
+    //         props.propsData[2].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Relationship') {
+    //         props.propsData[3].value = e.target.value;
+    //     }
+
+    //     if (e.target.name === 'Request to communicate in Spanish?') {
+    //         props.propsData[4].value = e.target.value;
+    //     }
+    // }
+    
+    // // Handles form submission to back end.
+    // const handleSubmit = (e) => {
+    //     // Tries to update database with an API call, catches error if one occurs.
+    //     if (saveConfirm === true) {
+    //         try {
+    //             nominationsAPI.updateActiveNomData(activeNomination.id, props.propsData);
+    //             console.log("success");
+    //         } catch (err) {
+    //             console.log(err);
+    //         }
+    //     }
+    // }
 
     return (
         <div className={styles.main}>
@@ -67,7 +87,7 @@ function ApplicationUpdateDetail(props) {
             </div>
             {
                 props.title == "Patient Information" ? <div>
-                    <form onSubmit={(e)=>handleSubmit(e)}>
+                    <form onSubmit={formik.handleSubmit}>
                         <h1></h1>
                         <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
                             {
@@ -85,7 +105,7 @@ function ApplicationUpdateDetail(props) {
                         </div>
                     </form>
                 </div>
-                    : <form>
+                    : <form onSubmit={formik.handleSubmit}>
                         <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
                             {
                                 props.propsData.map((obj) => (<div key={obj.label} className={obj.label === "" ? styles.mobileHide : ""}>
