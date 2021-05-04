5acec88e20597e2c8ec1cc1fca259ea70e72ba7c
Jon Westbrook <jon.westbrook.jr@gmail.com>
2021-05-03 20:34:34 -0500

    Getting closer to functional validation.

--- packages/app/src/components/nominationInfo/applicationUpdateDetail.js
+++ packages/app/src/components/nominationInfo/applicationUpdateDetail.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import styles from "./styles.module.css";
 import nominationsAPI from "./../../utils/API/nominationsAPI.js";
 import { ActiveNominationContext } from '../../utils/context/ActiveNominationContext';
-import { useFormik } from "formik"; // npm install formik --save
+import { Formik, Form, useField, useFormikContext } from "formik"; // npm install formik --save
 import * as Yup from 'yup'; // npm install yup --save
 
 /**
@@ -16,34 +16,7 @@ function ApplicationUpdateDetail(props) {
 
     const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
     const yesNoRegex = /^(?:Yes\b|No\b)/;
-    
-    // Formik object which handles form updates and submits changes.
-    const patientEditForm = () => {
-        const formik = useFormik({
-            initialValues: { // These initial values should be pulled from the objects in ../nominationInfo/index.js...how?
-                admissionDate: props.propsPatientData[2].value,
-                dischargeDate: props.propsPatientData[3].value,
-                repName: props.propsFamData[0].value,
-                email: props.propsFamData[1].value,
-                phoneNum: props.propsFamData[2].value,
-                relationship: props.propsFamData[3].value,
-                spanishComms: props.propsFamData[4].value,
-            },
-            // Yup handles validation in schema format. Confirm required function call does not matter for unchanged values.
-            validationSchema: Yup.object({
-                admissionDate: Yup.date().max(today).required('Required'),
-                dischargeDate: Yup.date().max(today).required('Required'),
-                repName: Yup.string().min(3, "Must be 3 characters or more.").max(30, 'Must be 30 characters or less.').required('Required'),
-                email: Yup.string().email('Invalid email address.').required('Required'), // This handles email validation with no regex.
-                phoneNum: Yup.string().matches(phoneRegex, "Please enter a valid phone number.").required('Required'),
-                relationship: Yup.string().min(3, "Must be at least 3 characters.").max(20, "Must be no more than 20 characters.").required('Required'),
-                spanishComms: Yup.string().matches(yesNoRegex, "Please enter yes or no.").required('Required'),
-            }),
-            onSubmit: values => {
-                alert(JSON.stringify(values, null, 2));
-            },
-        });
-    }
+
     
     return (
         <div className={styles.main}>
@@ -52,7 +25,29 @@ function ApplicationUpdateDetail(props) {
             </div>
             {
                 props.title == "Patient Information" ? <div>
-                    <form onSubmit={formik.handleSubmit}>
+                    <Formik initialValues = {{
+                        admissionDate: props.propsData[2].value,
+                        dischargeDate: props.propsData[3].value,
+                        repName: props.propsData[0].value,
+                        email: props.propsData[1].value,
+                        phoneNum: props.propsData[2].value,
+                        relationship: props.propsData[3].value,
+                        spanishComms: props.propsData[4].value,
+                    }} validationSchema = {Yup.object({
+                        admissionDate: Yup.date().required('Required'),
+                        dischargeDate: Yup.date().required('Required'),
+                        repName: Yup.string().min(3, "Must be 3 characters or more.").max(30, 'Must be 30 characters or less.').required('Required'),
+                        email: Yup.string().email('Invalid email address.').required('Required'), // This handles email validation with no regex.
+                        phoneNum: Yup.string().matches(phoneRegex, "Please enter a valid phone number.").required('Required'),
+                        relationship: Yup.string().min(3, "Must be at least 3 characters.").max(20, "Must be no more than 20 characters.").required('Required'),
+                        spanishComms: Yup.string().matches(yesNoRegex, "Please enter yes or no.").required('Required'),
+                    })}
+                    onSubmit={async (values, { setSubmitting }) => {
+                      await new Promise(r => setTimeout(r, 500));
+                      setSubmitting(false);
+                    }}
+                  >
+                      <Form>
                         <h1></h1>
                         <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
                             {
@@ -60,7 +55,7 @@ function ApplicationUpdateDetail(props) {
                                     {obj.label === "Admission Date" || obj.label === "Discharge Date" ?
                                         <div><label className={styles.label}>{obj.label}</label>
                                         {/* Need to tie label to value in next line somehow, and make functional for each label. */}
-                                            <input onChange={formik.handleChange} name={obj.label} type="date" defaultValue={obj.value} value={formik.values.admissionDate}/></div> :
+                                            <input name={obj.label} type="date" defaultValue={obj.value} /></div> :
                                         <div>
                                             <label className={styles.label}>{obj.label}</label>
                                             <span className={styles.value}>{String(obj.value)}</span>
@@ -69,19 +64,20 @@ function ApplicationUpdateDetail(props) {
                                 </div>))
                             }
                         </div>
-                    </form>
+                    </Form>
+                    </Formik>
                 </div>
                 // False condition which does not change any form data.
-                    : <form onSubmit={formik.handleSubmit}>
+                    : <Form>
                         <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
                             {
                                 props.propsData.map((obj) => (<div key={obj.label} className={obj.label === "" ? styles.mobileHide : ""}>
                                     <label className={styles.label}>{obj.label}</label>
-                                    <input name={obj.label} onChange={formik.handleChange} type="text" defaultValue={obj.value} />
+                                    <input name={obj.label} type="text" defaultValue={obj.value} />
                                 </div>))
                             }
                         </div>
-                    </form>
+                    </Form>
             }
         </div>
     );
