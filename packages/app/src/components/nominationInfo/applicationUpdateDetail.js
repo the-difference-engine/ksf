3c53c8154800b324099e3030131ef10e6342d788
Jon Westbrook <jon.westbrook.jr@gmail.com>
2021-05-12 20:27:35 -0500

    useEffect added.

--- packages/app/src/components/nominationInfo/applicationUpdateDetail.js
+++ packages/app/src/components/nominationInfo/applicationUpdateDetail.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import styles from "./styles.module.css";
 import nominationsAPI from "./../../utils/API/nominationsAPI.js";
 import { ActiveNominationContext } from '../../utils/context/ActiveNominationContext';
+import { useForm } from 'react-hook-form'; // npm install react-hook-form
 
 /**
  * Function which is called to update the current active nomination's data fields.
@@ -11,54 +12,17 @@ import { ActiveNominationContext } from '../../utils/context/ActiveNominationCon
  */
 function ApplicationUpdateDetail(props, hasBeenClicked) {
     const [activeNomination, setActiveNomination] = useContext(ActiveNominationContext);
-    let saveConfirm = false;
-    // Directly manipulates corresponding props values based on if statement checks.
-    const handleChange = (e) => {
-        // Prevents infinite change loop.
-        e.preventDefault();
-        saveConfirm = true;
-
-        // e.target.name checks props obj name string
-        if (e.target.name === 'Admission Date') {
-            props.propsData[2].value = e.target.value;
-        }
-
-        if (e.target.name === 'Discharge Date') {
-            props.propsData[3].value = e.target.value;
-        }
-
-        if (e.target.name === 'Representative Name') {
-            props.propsData[0].value = e.target.value;
-        }
-
-        if (e.target.name === 'Email Address') {
-            props.propsData[1].value = e.target.value;
-        }
-
-        if (e.target.name === 'Phone Number') {
-            props.propsData[2].value = e.target.value;
-        }
-
-        if (e.target.name === 'Relationship') {
-            props.propsData[3].value = e.target.value;
-        }
-
-        if (e.target.name === 'Request to communicate in Spanish?') {
-            props.propsData[4].value = e.target.value;
-        }
-    }
+    const { register, handleSubmit } = useForm();
     
-    const handleSubmit = (e) => {
-        // Tries to update database with an Axios call, catches error if one occurs.
-        if (saveConfirm === true) {
+    useEffect(() => {
+        if (hasBeenClicked) {
             try {
-                nominationsAPI.updateActiveNomData(activeNomination.id, props.propsData);
-                console.log("success");
+                nominationsAPI.updateActiveNomData(activeNomination.id, props);
             } catch (err) {
                 console.log(err);
             }
         }
-    }
+    });
 
     return (
         <div className={styles.main}>
@@ -67,14 +31,21 @@ function ApplicationUpdateDetail(props, hasBeenClicked) {
             </div>
             {
                 props.title == "Patient Information" ? <div>
-                    <form onSubmit={(e)=>handleSubmit(e)}>
+                    <form>
                         <h1></h1>
                         <div className={[styles.content, (props.gridContent && styles["grid-container"])].join(" ")}>
                             {
-                                props.propsData.map((obj) => (<div key={obj.label} className={obj.label === "" ? styles.mobileHide : ""}>
+                                props.propsData.map((obj) => (
+                                    <div key={obj.label} className={obj.label === "" ? styles.mobileHide : ""}>
                                     {obj.label === "Admission Date" || obj.label === "Discharge Date" ?
                                         <div><label className={styles.label}>{obj.label}</label>
-                                            <input onChange={(e) => handleChange(e)} name={obj.label} type="date" defaultValue={obj.value} /></div> :
+                                            <input 
+                                                name={obj.label} 
+                                                type="date" 
+                                                defaultValue={obj.value} 
+                                            />
+                                        </div> 
+                                        :
                                         <div>
                                             <label className={styles.label}>{obj.label}</label>
                                             <span className={styles.value}>{String(obj.value)}</span>
