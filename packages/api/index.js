53fcb5f0fcecc10ae116e23e7a5dc5078f903a20
Abraham Herrera <50112365+municipalvice@users.noreply.github.com>
2021-05-20 20:24:43 -0500

    Removed Scheduler references

--- packages/api/index.js
+++ packages/api/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const sequelize = require('./helper/sequelize.js');
 const app = require('./server');
 const PORT = process.env.PORT || 8080;
-const scheduler = require('./helper/scheduler.js')
 
 async function testSequelizeConnection() {
   try {
@@ -15,6 +14,4 @@ async function testSequelizeConnection() {
   }
 }
 testSequelizeConnection();
-process.env.TZ = 'America/Chicago'
-scheduler.reminders.start();
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
