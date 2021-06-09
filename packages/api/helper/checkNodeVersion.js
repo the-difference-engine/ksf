c72e89acc1382053424d7cb672687c90f222a799
Somers B Matthews <somersbmatthews@gmail.com>
2021-06-08 21:03:11 -0500

    added [api/helper/checkNodeVersion.js] and script code to api/package.json

--- packages/api/helper/checkNodeVersion.js
+++ packages/api/helper/checkNodeVersion.js
@@ -1,8 +1,8 @@
-import semver from 'semver';
-import { engines } from './package';
+const semver = require('semver')
+const pjson = require('../package.json')
 
-const version = engines.node;
+const version = pjson.engines.node;
 if (!semver.satisfies(process.version, version)) {
-  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
+  console.log(new Error(`Required node version ${version} not satisfied with current version ${process.version}.`));
   process.exit(1);
 }
