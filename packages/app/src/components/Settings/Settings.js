8bc87720395098f13749e0a1dc1a2bf347e76b5d
Taylor Keazirian <taylork26@gmail.com>
2021-07-28 20:59:55 -0500

    removed view applications column

--- packages/app/src/components/Settings/Settings.js
+++ packages/app/src/components/Settings/Settings.js
@@ -5,263 +5,275 @@ import grantCycleAPI from '../../utils/API/grantCycleAPI';
 import './styles.css';
 import EditModal from './EditModal';
 
-const Settings = (props) => {
-    const [newGrantCycle, setNewGrantCycle] = useState({ openedOn: "", closedOn: "", name: "" });
-    const [allGrantCycles, setGrantCycles] = useState([]);
-    const [disableButton, setDisableButton] = useState(true);
-    const [disableEditButton, setDisableEditButton] = useState(true);
-    const [errors, setErrors] = useState("");
-    const [editErrors, setEditErrors] = useState("");
-    const createButton = useRef(null);
-    const [activeGrantCycle, setActiveGrantCycle] = useState(null);
-    const [showEditModal, setShowEditModal] = useState(false);
-    const [gcToEdit, setGcToEdit] = useState();
+const Settings = props => {
+  const [newGrantCycle, setNewGrantCycle] = useState({
+    openedOn: '',
+    closedOn: '',
+    name: '',
+  });
+  const [allGrantCycles, setGrantCycles] = useState([]);
+  const [disableButton, setDisableButton] = useState(true);
+  const [disableEditButton, setDisableEditButton] = useState(true);
+  const [errors, setErrors] = useState('');
+  const [editErrors, setEditErrors] = useState('');
+  const createButton = useRef(null);
+  const [activeGrantCycle, setActiveGrantCycle] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [gcToEdit, setGcToEdit] = useState();
 
-    async function getGrantCycles() {
-        try {
-            const { data } = await grantCycleAPI.getGrantCycles();
-            setGrantCycles(data);
+  async function getGrantCycles() {
+    try {
+      const { data } = await grantCycleAPI.getGrantCycles();
+      setGrantCycles(data);
+    } catch (e) {
+      console.log('Error using getGrantCycles: ', e);
+    }
+  }
+
+  async function getActiveGrantCycle() {
+    try {
+      const { data } = await grantCycleAPI.getActiveGrantCycle();
+      setActiveGrantCycle(data);
+    } catch (e) {
+      console.log('Error using getActiveGrantCycle: ', e);
+    }
+  }
+
+  useEffect(() => {
+    getGrantCycles();
+    getActiveGrantCycle();
+  }, []);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setNewGrantCycle({ ...newGrantCycle, [input.name]: input.value });
+  };
+
+  const handleChangeForEdit = ({ currentTarget: input }) => {
+    setGcToEdit({ ...gcToEdit, [input.name]: input.value });
+  };
+
+  const handleCreate = async e => {
+    try {
+      const { data } = await grantCycleAPI.createGrantCycle(newGrantCycle);
+
+      setNewGrantCycle({ openedOn: '', closedOn: '', name: '' });
+      createButton.current.blur();
+      getGrantCycles();
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        setErrors(ex.response.data);
+        createButton.current.blur();
+      } else {
+        console.log(ex);
+      }
+    }
+  };
+
+  const handleUpdate = async e => {
+    try {
+      console.log(gcToEdit);
+      const { data } = await grantCycleAPI.updateGrantCycle(gcToEdit);
+
+      setGcToEdit({ openedOn: '', closedOn: '', name: '' });
+      getGrantCycles();
+      setShowEditModal(false);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        setEditErrors(ex.response.data);
+      } else {
+        console.log(ex);
+      }
+    }
+  };
+
+  const handleEdit = gc => {
+    setGcToEdit({
+      openedOn: gc.openedOn.split('T')[0],
+      closedOn: gc.closedOn.split('T')[0],
+      name: gc.name,
+      id: gc.id,
+    });
+    setShowEditModal(true);
+  };
+
+  useEffect(() => {
+    handleDisableButton(newGrantCycle);
+  }, [newGrantCycle]);
+
+  useEffect(() => {
+    if (gcToEdit) handleDisableEditButton(gcToEdit);
+  }, [gcToEdit]);
+
+  const isDateEditable = gc => {
+    const millisecondsInADay = 86400000;
+    const startDate = new Date(gc.openedOn);
+    const endDate = new Date(gc.closedOn);
+
+    return endDate - startDate >= millisecondsInADay ? false : true;
+  };
+
+  const handleDisableButton = gc => {
+    if (gc.openedOn !== '' && gc.closedOn !== '') {
+      const editableDate = isDateEditable(gc);
+
+      setDisableButton(editableDate);
+      if (!editableDate) {
+        setErrors('');
+        if (gc.name === '') {
+          setDisableButton(true);
+          setErrors('Please enter a name for the grant cycle');
         }
-        catch (e) { 
-            console.log("Error using getGrantCycles: ", e);
+      } else setErrors('Start Date must be earlier than End Date');
+    } else {
+      setDisableButton(true);
+    }
+  };
+
+  const handleDisableEditButton = gc => {
+    if (gc.openedOn !== '' && gc.closedOn !== '') {
+      const editableDate = isDateEditable(gc);
+
+      setDisableEditButton(editableDate);
+      if (!editableDate) {
+        setEditErrors('');
+        if (gc.name === '') {
+          setDisableEditButton(true);
+          setEditErrors('Please enter a name for the grant cycle');
         }
+        const closed = DateTime.fromISO(gc.closedOn);
+        if (DateTime.now() > closed) {
+          setDisableEditButton(true);
+        }
+      } else {
+        setEditErrors('Start Date must be earlier than End Date');
+      }
+    } else {
+      setDisableEditButton(true);
     }
 
-    async function getActiveGrantCycle() {
-        try {
-            const { data } = await grantCycleAPI.getActiveGrantCycle();
-            setActiveGrantCycle(data);
-        }
-        catch (e) {
-            console.log("Error using getActiveGrantCycle: ", e);
-         }
+    if (disableDatePicker(gc.closedOn)) {
+      setEditErrors('Cannot set date earlier than today.');
+      setDisableEditButton(true);
     }
+  };
 
-    useEffect(() => {
-        getGrantCycles();
-        getActiveGrantCycle();
-    }, []);
+  const closeEditModal = () => {
+    setShowEditModal(false);
+  };
 
-    const handleChange = ({ currentTarget: input }) => {
-        setNewGrantCycle({ ...newGrantCycle, [input.name]: input.value });
-    }
+  const disableDatePicker = d => {
+    const now = DateTime.now();
 
-    const handleChangeForEdit = ({ currentTarget: input }) => {
-        setGcToEdit({ ...gcToEdit, [input.name]: input.value });
-    }
+    return now > DateTime.fromISO(d);
+  };
 
-    const handleCreate = async (e) => {
-        try {
-            const { data } = await grantCycleAPI.createGrantCycle(newGrantCycle);
+  return (
+    <div className='settings__container'>
+      <EditModal
+        show={showEditModal}
+        handleClose={closeEditModal}
+        gc={gcToEdit}
+        disableButton={disableEditButton}
+        errors={editErrors}
+        handleChange={handleChangeForEdit}
+        onSubmit={handleUpdate}
+      />
+      <header className='settings__header'>
+        <h1 className='settings__title'>Settings</h1>
+      </header>
 
-            setNewGrantCycle({ openedOn: "", closedOn: "", name: "" })
-            createButton.current.blur();
-            getGrantCycles();
-        }
-        catch (ex) {
-            if (ex.response && ex.response.status === 400) {
-                setErrors(ex.response.data);
-                createButton.current.blur();
-            }
-            else {
-                console.log(ex);
-            }
-        }
-    }
+      <aside className='settings__sidebar'>
+        <ul className='settings__list'>
+          <li className='settings__list-item'>Grant Cycle</li>
+        </ul>
+      </aside>
 
-    const handleUpdate = async (e) => {
-
-        try {
-            console.log(gcToEdit);
-            const { data } = await grantCycleAPI.updateGrantCycle(gcToEdit);
-
-            setGcToEdit({ openedOn: "", closedOn: "", name: "" })
-            getGrantCycles();
-            setShowEditModal(false);
-        }
-        catch (ex) {
-            if (ex.response && ex.response.status === 400) {
-                setEditErrors(ex.response.data);
-            }
-            else {
-                console.log(ex);
-            }
-        }
-    }
-
-    const handleEdit = gc => {
-        setGcToEdit({ openedOn: gc.openedOn.split("T")[0], closedOn: gc.closedOn.split("T")[0], name: gc.name, id: gc.id });
-        setShowEditModal(true);
-    }
-
-    useEffect(() => {
-        handleDisableButton(newGrantCycle);
-    }, [newGrantCycle])
-
-    useEffect(() => {
-        if (gcToEdit)
-            handleDisableEditButton(gcToEdit);
-    }, [gcToEdit])
-
-    const isDateEditable = (gc) => {
-        const millisecondsInADay = 86400000;
-        const startDate = new Date(gc.openedOn);
-        const endDate = new Date(gc.closedOn);
-        
-        return endDate - startDate >= millisecondsInADay ? false : true;
-    }
-
-    const handleDisableButton = (gc) => {
-        if (gc.openedOn !== "" && gc.closedOn !== "") {
-            const editableDate = isDateEditable(gc);
-
-            setDisableButton(editableDate);
-            if (!editableDate) {
-                setErrors("")
-                if (gc.name === "") {
-                    setDisableButton(true);
-                    setErrors("Please enter a name for the grant cycle");
-                }
-            }
-            else setErrors("Start Date must be earlier than End Date");
-        }
-        else {
-            setDisableButton(true);
-        }
-    }
-
-    const handleDisableEditButton = (gc) => {
-        if (gc.openedOn !== "" && gc.closedOn !== "") {
-            const editableDate = isDateEditable(gc);
-
-            setDisableEditButton(editableDate);
-            if (!editableDate) {
-                setEditErrors("")
-                if (gc.name === "") {
-                    setDisableEditButton(true);
-                    setEditErrors("Please enter a name for the grant cycle");
-
-                }
-                const closed = DateTime.fromISO(gc.closedOn);
-                if (DateTime.now() > closed) {
-                    setDisableEditButton(true);
-                }
-            }
-            else {
-                setEditErrors("Start Date must be earlier than End Date");
-            }
-        }
-
-        else {
-            setDisableEditButton(true);
-        }
-
-        if (disableDatePicker(gc.closedOn)) {
-            setEditErrors("Cannot set date earlier than today.");
-            setDisableEditButton(true);
-        }
-    }
-
-    const closeEditModal = () => {
-        setShowEditModal(false);
-    }
-
-    const disableDatePicker = (d) => {
-        const now = DateTime.now();
-
-        return now > DateTime.fromISO(d);
-    }
-
-    return (
-        <div className="settings__container">
-            <EditModal
-                show={showEditModal}
-                handleClose={closeEditModal}
-                gc={gcToEdit}
-                disableButton={disableEditButton}
-                errors={editErrors}
-                handleChange={handleChangeForEdit}
-                onSubmit={handleUpdate}
-            />
-            <header className="settings__header">
-                <h1 className="settings__title">
-                    Settings
-                </h1>
-            </header>
-
-            <aside className="settings__sidebar">
-                <ul className="settings__list">
-                    <li className="settings__list-item">Grant Cycle</li>
-                </ul>
-            </aside>
-
-            <main className="settings__main">
-                <h2 className="settings__heading">Create Grant Cycle</h2>
-                <div className="settings__form">
-                    <div className="settings__input-block">
-                        <p className="settings__input-label">Start Date:</p>
-                        <span className="settings__input">
-                            <input
-                                value={newGrantCycle.openedOn}
-                                name="openedOn"
-                                onChange={handleChange}
-                                type="date"
-                            />
-                        </span>
-                    </div>
-                    <div className="settings__input-block">
-                        <p className="settings__input-label">End Date:</p>
-                        <span className="settings__input">
-                            <input
-                                value={newGrantCycle.closedOn}
-                                name="closedOn"
-                                onChange={handleChange}
-                                type="date"
-                            />
-                        </span>
-                    </div>
-                    <div className="settings__input-block">
-                        <p className="settings__input-label">Cycle Name:</p>
-                        <span className="settings__input">
-                            <input
-                                value={newGrantCycle.name}
-                                name="name"
-                                onChange={handleChange}
-                                type="text"
-                            />
-                        </span>
-                    </div>
-                </div>
-                <button ref={createButton} disabled={disableButton} onClick={handleCreate} className="settings__button">Create</button>
-                <div className="settings__form-errors">{errors}</div>
-
-                <div>
-                    <h2 className="settings__heading">Active Grant Cycle: &nbsp;&nbsp;&nbsp;&nbsp;{activeGrantCycle ? <span className="settings__activeGrantCycle">{activeGrantCycle.name}</span> : <span className="settings__activeGrantCycle">None</span>}</h2>
-                </div>
-
-                <h2 className="settings__heading">Grant Cycle History</h2>
-                <div className="settings__table-wrapper">
-                    <table className="settings__table">
-                        <thead className="settings__thead">
-                            <tr>
-                                <th>Start Date</th>
-                                <th>End Date</th>
-                                <th>Cycle Name</th>
-                                <th className="settings__th-column">Applications<br />Ready for Board Review</th>
-                                <th>View<br />Applications</th>
-                            </tr>
-                        </thead>
-                        <tbody className="settings__tbody">
-                            {allGrantCycles.map(gc => (
-                                <TableRow key={gc.id} grantCycle={gc} onResultsClick={props.onResultsClick} onEdit={handleEdit} />
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </main>
-
+      <main className='settings__main'>
+        <h2 className='settings__heading'>Create Grant Cycle</h2>
+        <div className='settings__form'>
+          <div className='settings__input-block'>
+            <p className='settings__input-label'>Start Date:</p>
+            <span className='settings__input'>
+              <input
+                value={newGrantCycle.openedOn}
+                name='openedOn'
+                onChange={handleChange}
+                type='date'
+              />
+            </span>
+          </div>
+          <div className='settings__input-block'>
+            <p className='settings__input-label'>End Date:</p>
+            <span className='settings__input'>
+              <input
+                value={newGrantCycle.closedOn}
+                name='closedOn'
+                onChange={handleChange}
+                type='date'
+              />
+            </span>
+          </div>
+          <div className='settings__input-block'>
+            <p className='settings__input-label'>Cycle Name:</p>
+            <span className='settings__input'>
+              <input
+                value={newGrantCycle.name}
+                name='name'
+                onChange={handleChange}
+                type='text'
+              />
+            </span>
+          </div>
         </div>
-    );
-}
+        <button
+          ref={createButton}
+          disabled={disableButton}
+          onClick={handleCreate}
+          className='settings__button'
+        >
+          Create
+        </button>
+        <div className='settings__form-errors'>{errors}</div>
+
+        <div>
+          <h2 className='settings__heading'>
+            Active Grant Cycle: &nbsp;&nbsp;&nbsp;&nbsp;
+            {activeGrantCycle ? (
+              <span className='settings__activeGrantCycle'>
+                {activeGrantCycle.name}
+              </span>
+            ) : (
+              <span className='settings__activeGrantCycle'>None</span>
+            )}
+          </h2>
+        </div>
+
+        <h2 className='settings__heading'>Grant Cycle History</h2>
+        <div className='settings__table-wrapper'>
+          <table className='settings__table'>
+            <thead className='settings__thead'>
+              <tr>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Cycle Name</th>
+                <th className='settings__th-column'>Applications</th>
+              </tr>
+            </thead>
+            <tbody className='settings__tbody'>
+              {allGrantCycles.map(gc => (
+                <TableRow
+                  key={gc.id}
+                  grantCycle={gc}
+                  onResultsClick={props.onResultsClick}
+                  onEdit={handleEdit}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default Settings;
--- packages/app/src/components/Settings/TableRow.js
+++ packages/app/src/components/Settings/TableRow.js
@@ -1,39 +1,73 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DateTime } from 'luxon';
-import { formatDateString } from '../../utils/formatDateString'
+import { formatDateString } from '../../utils/formatDateString';
 
+const TableRow = props => {
+  const { grantCycle, onEdit, show, onResultsClick } = props;
 
-const TableRow = (props) => {
-    const { grantCycle, onEdit, show, onResultsClick } = props;
+  const timeDiff = dateString => {
+    const date = DateTime.fromISO(dateString);
+    const now = DateTime.now().startOf('day');
 
-    const timeDiff = (dateString) => {
-        const date = DateTime.fromISO(dateString);
-        const now = DateTime.now().startOf('day');
+    return date > now;
+  };
 
-        return date > now;
-    }
-    
-    return (
-        <tr>
-            <td>
-                { formatDateString(grantCycle.openedOn) } &nbsp;&nbsp;
-                {timeDiff(grantCycle.openedOn) ? <FontAwesomeIcon onClick={() => onEdit(grantCycle)} icon="pencil-alt" className="icon-table"/> : ""}
-                
-            </td>
-            <td>
-                { formatDateString(grantCycle.closedOn) } &nbsp;&nbsp;
-                {timeDiff(grantCycle.closedOn) ? <FontAwesomeIcon onClick={() => onEdit(grantCycle)} icon="pencil-alt" className="icon-table"/> : ""}
-            </td>
-            <td> { grantCycle.name } </td>
-            <td style={{width: '250px'}}>
-                { grantCycle.nominations.length } &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                
-            </td>
-            <td><FontAwesomeIcon icon="arrow-right" onClick={() => onResultsClick(grantCycle)} size="1x" className={grantCycle.nominations.length ?"icon-table icon-right-arrow" : "icon-right-arrow-disabled" }/></td>
-        </tr>
-    )
+  return (
+    <tr>
+      <td>
+        {formatDateString(grantCycle.openedOn)} &nbsp;&nbsp;
+        {timeDiff(grantCycle.openedOn) ? (
+          <FontAwesomeIcon
+            onClick={() => onEdit(grantCycle)}
+            icon='pencil-alt'
+            className='icon-table'
+          />
+        ) : (
+          ''
+        )}
+      </td>
+      <td>
+        {formatDateString(grantCycle.closedOn)} &nbsp;&nbsp;
+        {timeDiff(grantCycle.closedOn) ? (
+          <FontAwesomeIcon
+            onClick={() => onEdit(grantCycle)}
+            icon='pencil-alt'
+            className='icon-table'
+          />
+        ) : (
+          ''
+        )}
+      </td>
+      <td> {grantCycle.name} </td>
+      <td style={{ width: '250px' }}>
+        {/* number of applications displayed */}
+        {/* {grantCycle.nominations.length} */}
+        <button
+          onClick={() => onResultsClick(grantCycle)}
+          className={
+            grantCycle.nominations.length
+              ? 'icon-table icon-right-arrow'
+              : 'icon-right-arrow-disabled'
+          }
+        >
+          {grantCycle.nominations.length}
+        </button>
+      </td>
+      {/* <td>
+        <FontAwesomeIcon
+          icon='arrow-right'
+          onClick={() => onResultsClick(grantCycle)}
+          size='1x'
+          className={
+            grantCycle.nominations.length
+              ? 'icon-table icon-right-arrow'
+              : 'icon-right-arrow-disabled'
+          }
+        />
+      </td> */}
+    </tr>
+  );
+};
 
-}
- 
 export default TableRow;
