54eb636a7683b1ba004f0efbd931cd19683760ce
Somers B Matthews <somersbmatthews@gmail.com>
2021-08-02 19:06:30 -0500

    fixed remaining merge conflict in [/api/controllers/nomination.js]

--- packages/api/controllers/nomination.js
+++ packages/api/controllers/nomination.js
@@ -9,12 +9,8 @@ const { sendDeclineEmail } = require('../helper/mailer');
 const { verifyHcEmail } = require('../helper/mailer');
 const { sendSurveyReminder } = require('../helper/mailer');
 const { sendHIPAAReminder } = require('../helper/mailer');
-<<<<<<< HEAD
-const { sendHIPAAEmail } = require('../helper/mailer');
-=======
 const { sendHIPAAEmail} = require('../helper/mailer');
 const { sendSurveySocialWorker } = require('../helper/mailer');
->>>>>>> 796025029010531b7bd7dabbdf1cf6ec9385de53
 const gsheetToDB = require('../helper/nominationGsheetToDB');
 const jwt = require('jsonwebtoken');
 const Op = sequelize.Op;
