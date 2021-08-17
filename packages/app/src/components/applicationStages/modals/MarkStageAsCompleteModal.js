5ff5ea548034b965c768f76a6c3d7c3845b78e9f
Taylor Keazirian <taylork26@gmail.com>
2021-08-16 19:39:25 -0500

    updated button style in css to stay green

--- packages/app/src/components/applicationStages/modals/MarkStageAsCompleteModal.js
+++ packages/app/src/components/applicationStages/modals/MarkStageAsCompleteModal.js
@@ -3,26 +3,29 @@ import Modal from 'react-modal';
 import { useContext } from 'react';
 import { ActiveNominationContext } from '../../../utils/context/ActiveNominationContext';
 
-
-
 Modal.setAppElement('#root');
 
 const MarkStageAsCompleteModal = ({ advanceStage, currentStatus }) => {
-    const [activeNomination, setActiveNomination] = useContext(ActiveNominationContext);
-    const [isOpen, setIsOpen] = useState(false);
-    
-    
-    function openModal() {
-      setIsOpen(true);
-    };
+  const [activeNomination, setActiveNomination] = useContext(
+    ActiveNominationContext
+  );
+  const [isOpen, setIsOpen] = useState(false);
 
-    function closeModal() {
-      setIsOpen(false);
-    }   
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div>
-      <button disabled = {activeNomination.status == "Declined"} className="button next" onClick={openModal}>
+      <button
+        disabled={activeNomination.status === 'Declined'}
+        className="button next"
+        onClick={openModal}
+      >
         <span>&#10003;</span>Mark Stage as Complete
       </button>
       <Modal
