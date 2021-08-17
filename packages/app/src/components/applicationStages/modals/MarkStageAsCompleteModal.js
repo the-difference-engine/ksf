4b0d95a5954e3d1ad56fdd1cc2c96965c8938421
Taylor Keazirian <taylork26@gmail.com>
2021-08-16 19:39:25 -0500

    updated button style in css to stay green

--- packages/app/src/components/applicationStages/modals/MarkStageAsCompleteModal.js
+++ packages/app/src/components/applicationStages/modals/MarkStageAsCompleteModal.js
@@ -3,26 +3,29 @@ import Modal from 'react-modal';
 import { useContext } from 'react';
 import { ActiveNominationContext } from '../../../utils/context/ActiveNominationContext';
 
-
-
 Modal.setAppElement('#root');
 
 const MarkStageAsCompleteModal = ({ advanceStage, currentStatus }) => {
-    const [activeNomination, setActiveNomination] = useContext(ActiveNominationContext);
-    const [isOpen, setIsOpen] = useState(false);
-    
-    
-    function openModal() {
-      setIsOpen(true);
-    };
+  const [activeNomination, setActiveNomination] = useContext(
+    ActiveNominationContext
+  );
+  const [isOpen, setIsOpen] = useState(false);
 
-    function closeModal() {
-      setIsOpen(false);
-    }   
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div>
-      <button disabled = {activeNomination.status == "Declined"} className="button next" onClick={openModal}>
+      <button
+        disabled={activeNomination.status === 'Declined'}
+        className="button next"
+        onClick={openModal}
+      >
         <span>&#10003;</span>Mark Stage as Complete
       </button>
       <Modal
@@ -32,14 +35,26 @@ const MarkStageAsCompleteModal = ({ advanceStage, currentStatus }) => {
         onRequestClose={closeModal}
         contentLabel="Mark Stage as Complete"
       >
-        <div className='modal-close-button' onClick={closeModal}>&times;</div>
-          <h3 className='modal-content-text'>Do you want to mark stage as complete?</h3>
-        <div className='modal-footer'> 
-          <button className='modal-button-yes' onClick={() => {
-            advanceStage(currentStatus);
-            closeModal();
-          }}>Yes</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button className="modal-button-no" onClick={closeModal}>No</button>
+        <div className="modal-close-button" onClick={closeModal}>
+          &times;
+        </div>
+        <h3 className="modal-content-text">
+          Do you want to mark stage as complete?
+        </h3>
+        <div className="modal-footer">
+          <button
+            className="modal-button-yes"
+            onClick={() => {
+              advanceStage(currentStatus);
+              closeModal();
+            }}
+          >
+            Yes
+          </button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button className="modal-button-no" onClick={closeModal}>
+            No
+          </button>
         </div>
       </Modal>
     </div>
