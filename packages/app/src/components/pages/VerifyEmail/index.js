e37b949fb592e9ab4fede0a53401666f098b3967
John Leavell <johnleavell@gmail.com>
2021-08-31 00:56:26 -0500

    testing route options

--- packages/app/src/components/pages/VerifyEmail/index.js
+++ packages/app/src/components/pages/VerifyEmail/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import nominationsAPI from '../../../utils/API/nominationsAPI';
 
 const VerifyEmail = () => {
+    function sendValidationEmail () {
+        nominationsAPI.validateEmail()
+    }
+
     return (
         <div>
             <h2>Thank you for verifying your email!</h2>
