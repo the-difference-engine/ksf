4f1e86b5453072d142f4acca0195f5bf210c1ec7
John Leavell <johnleavell@gmail.com>
2021-09-01 19:49:35 -0500

    removed stray comma

--- packages/app/src/components/pages/VerifyEmail/index.js
+++ packages/app/src/components/pages/VerifyEmail/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import nominationsAPI from '../../../utils/API/nominationsAPI';
+
 
 const VerifyEmail = () => {
-    function sendValidationEmail () {
-        nominationsAPI.validateEmail()
-    }
 
     return (
         <div>
