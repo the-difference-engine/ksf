1188ffcc7174447bdf2036fdf3dbfc6708bee19f
floresjmjr <floresjmjr@gmail.com>
2021-09-06 21:31:33 -0400

    verifyemail is rendered and then api call is made

--- packages/app/src/components/pages/VerifyEmail/index.js
+++ packages/app/src/components/pages/VerifyEmail/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import axios from 'axios';
 
-const VerifyEmail = (props) => {
+class VerifyEmail extends React.Component {
+  
+    state = {
+        API_URL:  process.env.REACT_APP_API_URL,
+        token: this.props.match.params.token,
+    }
 
-    const API_URL = process.env.REACT_APP_API_URL;
-    const token = props.match.params.token;
+    componentDidMount() {
+        axios.get(`${this.state.API_URL}/api/confirmation/${this.state.token}`)
+    }
 
-    axios.get(`${API_URL}/api/confirmation/${token}`)
-
-    return (
-        <div>
-            <h4>Thank you for verifying your email!</h4>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div>
+                <h4>Thank you for verifying your email!</h4>
+            </div>
+        )
+    }
+}    
 
 export default VerifyEmail;
