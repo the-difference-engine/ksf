70ed8d8d38f80ed0ae17758308840cf381d292c2
John Leavell <johnleavell@gmail.com>
2021-09-13 20:29:00 -0500

    Ticket173 (#333)
    * displayed test email and clickable link
    
    * nominations api, and nominations controller, working on jwt
    
    * created pug file for verification link
    
    * verifyHcEmail page skeleton
    
    * restart project to merge conflicts
    
    * added redirect route and ownership message
    
    * editing createNomination function
    
    * ticket requirements complete
    
    * cleaned up code
    
    * cleaned up json.lock error
    
    * removed stray comments, changed the redirect to a response
    
    * testing email verification
    
    * removed stary comment
    
    * testing
    
    * testing route options
    
    * editing styling on email verification page
    
    * removed stray comments
    
    * removed stray comma
    
    * updated nomination test file to reflect redirect changes
    
    * controller method returns status 200, mailer method provides fron-end route, transporter preview false, changes in the nomination test file relating to verification
    
    * updated verification route, updated verification component to have it call API
    
    * verifyemail is rendered and then api call is made
    
    * converted verify component from class to function, removed console log from mailer file, changed status code from 200 to 204 in nomination file and test file
    
    * updated VerifyEmail component to use utils nomination API method
    
    * changed get request to post
    
    * removed useState from the verifyEmail component
    
    * updated from get to post in nomination test file
    
    * copied package-lock.json, api package-lock.json, app package-lock.json from develop
    
    * made minor formatting changes to the VerifyEmail component regarding spacing and newlines
    
    Co-authored-by: floresjmjr <floresjmjr@gmail.com>

--- packages/api/helper/generateToken.js
+++ packages/api/helper/generateToken.js
@@ -1,10 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (nomination) => {
-  return jwt.sign({ nomination }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
-  });
-};
+const generateToken = (id) => jwt.sign({ data: id }, process.env.JWT_SECRET, {
+  expiresIn: '1d',
+});
 
 module.exports = {
   generateToken,
