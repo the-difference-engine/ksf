91fcb2e5b2b72a4ea14db4dcd19908fcb1a40479
John Leavell <johnleavell@gmail.com>
2021-10-09 19:21:23 -0500

    updated styling

--- packages/app/src/components/Settings/BlockedDomainsTab.js
+++ packages/app/src/components/Settings/BlockedDomainsTab.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import blockedDomainsList from '../../utils/blockedDomainsList';
 
-
 const BlockedDomainsTab = () => {
   const [domainValue, setDomainValue] = useState('');
 
   const handleChange = (e) => {
     setDomainValue(e.currentTarget.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    blockedDomainsList.push(domainValue)
+    blockedDomainsList.push(domainValue);
     console.log('clicket----------');
   };
 
@@ -19,12 +18,8 @@ const BlockedDomainsTab = () => {
   return (
     <main className="settings__main">
       <h2 className="settings__heading">Blocked Domains</h2>
-      <input 
-        type='text'
-        value={domainValue}
-        onChange={handleChange}
-        />
-        <button onClick={handleSubmit}>Add Domain</button>
+      <input type="text" value={domainValue} onChange={handleChange} />
+      <button onClick={handleSubmit}>Add Domain</button>
       <ul>
         {blockedDomainsList.map((domain) => {
           return <li>{domain}</li>;
