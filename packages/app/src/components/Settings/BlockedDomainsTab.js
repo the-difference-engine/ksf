6fd4f579e0d60383563e648b5b19e26489108806
John Leavell <johnleavell@gmail.com>
2021-10-09 19:19:28 -0500

    added text field and add domain button

--- packages/app/src/components/Settings/BlockedDomainsTab.js
+++ packages/app/src/components/Settings/BlockedDomainsTab.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import blockedDomainsList from '../../utils/blockedDomainsList';
 
+
 const BlockedDomainsTab = () => {
+  const [domainValue, setDomainValue] = useState('');
+
+  const handleChange = (e) => {
+    setDomainValue(e.currentTarget.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    blockedDomainsList.push(domainValue)
+    console.log('clicket----------');
+  };
+
+  console.log(domainValue, '---------------DominValue-=');
   return (
     <main className="settings__main">
       <h2 className="settings__heading">Blocked Domains</h2>
+      <input 
+        type='text'
+        value={domainValue}
+        onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Add Domain</button>
       <ul>
         {blockedDomainsList.map((domain) => {
           return <li>{domain}</li>;
